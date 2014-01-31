94bd44062507a2182a1beaf2240239b82ecaf5d9
Matt Torok <matt.torok@gmail.com>
2014-01-31 15:17:26 -0800

    Naive n-body: remove debugging code, clean up comments

--- shaders/cl-nbody.c
+++ shaders/cl-nbody.c
@@ -25,30 +25,44 @@
 * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+
+// For a single point in the graph, calculate the forces from each other point in the graph on it,
+// and write out an updated position and velocity for it.
 __kernel void nbody_kernel_GPU(
-    __global float* curPos,
-    __global float* curVel,
-    int numBodies,
-    float deltaTime,
-    int epsSqr,
-    __local float* localPos,
-    __global float* nxtPos,
-    __global float* nxtVel)
+    __global float* curPos,     // Input position (read-only)
+    __global float* curVel,     // Input velocities (read-only)
+    int numBodies,              // Total number of points
+    float deltaTime,            // Always 0.005 currently
+    int epsSqr,                 // Always 50 currently
+    __local float* localPos,    // Array at least big enough to hold one point from each workgroup
+    __global float* nxtPos,     // Output positions (write-only)
+    __global float* nxtVel)     // Output velocities (write-only)
 {
     unsigned int tid = get_local_id(0);
     unsigned int gid = get_global_id(0);
+    // Threads in this workgroup
     unsigned int localSize = get_local_size(0);
 
-    // Number of tiles we need to iterate
+    // Number of tiles we need to iterate, i.e. the number of workgroups
     unsigned int numTiles = numBodies / localSize;
 
-    // position of this work-item
+    // position of this work-item, i.e. the point we're updating
     float4 myPos = (float4) (curPos[4*gid + 0], curPos[4*gid + 1], curPos[4*gid + 2], curPos[4*gid + 3]);
     float4 acc = (float4) (0.0f, 0.0f, 0.0f, 0.0f);
 
+    // For optimized memory access, we break up the set of points we're currently considering into
+    // 'tiles'. The number of points in a tile is equal to the number of threads each workgroup has,
+    // so we use all the threads in this workgroup to fetch a point from the tile into local memory,
+    // sync, calculate the forces from all those points, sync, then fetch the next tile, and repeat.
     for(int i = 0; i < numTiles; ++i)
     {
-        // load one tile into local memory
+        // The syncs in this loop mean that all threads in this workgroup are operating on the same
+        // tile at the same time.
+        
+        // For the current tile, grab one point from it. Because there are an equal number of points
+        // in a tile as there are threads in a workgroup (all workgroups have the same # of threads,
+        // which are even divisible by the total # of threads/points,) when all threads are done,
+        // all points from that workgroup have been fetched into local memory.
         int idx = i * localSize + tid;
         for(int k=0; k<4; k++)
         {
@@ -56,7 +70,8 @@ __kernel void nbody_kernel_GPU(
         }
         // Synchronize to make sure data is available for processing
         barrier(CLK_LOCAL_MEM_FENCE);
-        // calculate acceleration effect due to each body
+        
+        // For all the points from the current tile, calculate acceleration effect due to each body
         // a[i->j] = m[j] * r[i->j] / (r^2 + epsSqr)^(3/2)
         for(int j = 0; j < localSize; ++j)
         {
@@ -77,9 +92,9 @@ __kernel void nbody_kernel_GPU(
     float4 oldVel = (float4) (curVel[4*gid + 0], curVel[4*gid + 1], curVel[4*gid + 2], curVel[4*gid + 3]);
 
     // updated position and velocity
-    float4 newPos = myPos + oldVel * deltaTime + acc * 0.5f * deltaTime * deltaTime;
+    float4 newPos = myPos + (oldVel * deltaTime) + (acc * 0.5f * (deltaTime * deltaTime));
     newPos.w = myPos.w;
-    float4 newVel = oldVel + acc * deltaTime;
+    float4 newVel = oldVel + (acc * deltaTime);
 
     // check boundry
     if(newPos.x > 1.0f || newPos.x < -1.0f || newPos.y > 1.0f || newPos.y < -1.0f || newPos.z > 1.0f || newPos.z < -1.0f) {
