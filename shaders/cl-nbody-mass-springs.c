3e6d382c3c5ca15529e794cd17cfd1c060e0cac5
Matt Torok <matt@graphistry.com>
2014-02-06 05:37:39 -0800

    Naive: point repulsion now working (sans wall force)
    Points should now animate, repulsing from one another. The rate is controlled by the POINT_REPULSION macro in the .cl file, along with the time delta passed into the kernel by SimCL.
    
    Wall force (where points are also repulsed by forces emanating from each of the four walls bounding the graph) is not yet implemented, so most points will end up in the corners at the moment.

--- shaders/cl-nbody-mass-springs.c
+++ shaders/cl-nbody-mass-springs.c
@@ -1,11 +1,14 @@
 // Number of elements per 2d point
 #define COMPONENTS_2D 2
 
-#define POINT_REPULSION 0.00001
+#define POINT_REPULSION 1.0
 #define EDGE_REPULSION 0.5
 
 #define SPRING_LENGTH 0.1
 #define SPRING_FORCE 0.1
+
+// TODO: Add in a repulsive force from the four walls
+// TODO: Allow the wall coordinates to be passed as a kernel argument
 
 __kernel void nbody_compute_repulsion(
 	unsigned int numPoints,
@@ -31,41 +34,65 @@ __kernel void nbody_compute_repulsion(
 	// Points per tile = threads per workgroup
 	unsigned int tileSize = get_local_size(0);
 	unsigned int numTiles = numPoints / tileSize;
+	numTiles = max(numTiles, (uint) 1);
 
 	float2 posDelta = (float2) (0.0f, 0.0f);
 
 	for(int tile = 0; tile < numTiles; tile++) {
-		unsigned int tilePointId = (tile * tileSize) + threadLocalId;
+		unsigned int tileStart = (tile * (tileSize * COMPONENTS_2D));
+		unsigned int tilePointId = tileStart + (threadLocalId * COMPONENTS_2D);
 
 		// TODO: Convert to a bulk local, synchronized read
-		tilePoints[(COMPONENTS_2D * threadLocalId) + 0] = inputPositions[(COMPONENTS_2D * tilePointId) + 0];
-		tilePoints[(COMPONENTS_2D * threadLocalId) + 1] = inputPositions[(COMPONENTS_2D * tilePointId) + 1];
+		tilePoints[(COMPONENTS_2D * threadLocalId) + 0] = inputPositions[tilePointId + 0];
+		tilePoints[(COMPONENTS_2D * threadLocalId) + 1] = inputPositions[tilePointId + 1];
 
 		barrier(CLK_LOCAL_MEM_FENCE);
 
 		for(unsigned int j = 0; j < tileSize; j++) {
-			float2 otherPoint = (float2) (tilePoints[(COMPONENTS_2D * j) + 0], tilePoints[(COMPONENTS_2D * j) + 1]);
+			unsigned int cachedPoint = j * COMPONENTS_2D;
+			// Skip calculating forces if the other point is really this point (that is, don't
+			// calculate the forces of a point on itself.)
+			if(tileStart + cachedPoint == pointId) {
+				continue;
+			}
 
-			float2 dir = otherPoint - myPos;
+			float2 otherPoint = (float2) (tilePoints[cachedPoint + 0], tilePoints[cachedPoint + 1]);
+
+			// Calculate force as POINT_REPULSION * (1/(distance^2))
+			float dist = distance(myPos, otherPoint);
+			float2 dir;
+			float force;
+
+			if(dist > 0) {
+				// Force magnitude is POINT_REPULSION * (1/(distance^2))
+				force = POINT_REPULSION * (1.0/(dist*dist));
+				// Force direction is the direction of the other point
+				dir = otherPoint - myPos;
+			} else {
+				// If dist <= 0, leave force as 100% of FORCE_REPULSION
+				force = POINT_REPULSION;
+				// Force direction is set to a (sorta) random direction
+				dir = (float2) (pointId, pointId);
+			}
+
+			force = clamp(force, (float) 0.0, (float) POINT_REPULSION);
 			dir = normalize(dir);
 
-			float dist = distance(myPos, otherPoint);
-			float force = POINT_REPULSION * ((1.0/(dist*dist))/1000);
-
-			float2 change = dir * force * timeDelta;
+			float2 change = dir * force * timeDelta * -1;
 			posDelta += change;
 		}
-
-		myPos += posDelta;
-
-		// Clamp myPos to be within the walls
-		myPos.x = clamp(myPos.x, walls[0], walls[1]);
-		myPos.y = clamp(myPos.y, walls[2], walls[3]);
 
 		// Calculate force from walls
 
 		barrier(CLK_LOCAL_MEM_FENCE);
 	}
+
+
+	myPos += posDelta;
+
+	// Clamp myPos to be within the walls
+	myPos.x = clamp(myPos.x, walls[0], walls[1]);
+	myPos.y = clamp(myPos.y, walls[2], walls[3]);
 
 	// TODO: Convert to vector write
 	outputPositions[pointId + 0] = myPos.x;
