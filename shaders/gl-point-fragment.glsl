d3a5b790f4bed7d442fbf46a16c77f0546e32a53
Matt Torok <matt@graphistry.com>
2014-02-12 02:59:55 -0800

    Naive: remove dead code from GL shaders. Remove 3rd party copyright.
    90% of the code in the GL shader has been commented out. This commit removes that code, as it's unlikely to be useful with our current architecture.
    
    The original shaders were taken from the Samsung WebKit-WebCL project's WebCL example suite. Because of this, they carried a Samsung copyright notice and disclaimer. However, with the removal of dead code above, all that's left is the most basic remaining code, which is original work. Therefore, since all the original Samsung code is gone, we have removed the Samsung copyright notice.

--- shaders/gl-point-fragment.glsl
+++ shaders/gl-point-fragment.glsl
@@ -1,58 +1,6 @@
-/*
-* Copyright (C) 2011 Samsung Electronics Corporation. All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided the following conditions
-* are met:
-*
-* 1.  Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*
-* 2.  Redistributions in binary form must reproduce the above copyright
-*     notice, this list of conditions and the following disclaimer in the
-*     documentation and/or other materials provided with the distribution.
-*
-* THIS SOFTWARE IS PROVIDED BY SAMSUNG ELECTRONICS CORPORATION AND ITS
-* CONTRIBUTORS "AS IS", AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING
-* BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SAMSUNG
-* ELECTRONICS CORPORATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES(INCLUDING
-* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-* DATA, OR PROFITS, OR BUSINESS INTERRUPTION), HOWEVER CAUSED AND ON ANY THEORY
-* OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING
-* NEGLIGENCE OR OTHERWISE ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-* EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 precision mediump float;
 varying vec4 vColor;
+
 void main(void) {
-    // A GL point is just a square, but we want to draw circles. Calculate if the coordinate of this
-    // point (gl_PointCoord is in relation to the center of the point) is within the circle 0.5
-    // units in radius around the center.
-    // float dx = (gl_PointCoord.x - 0.5);
-    // float dy = (gl_PointCoord.y - 0.5);
-    // float r = sqrt(dx*dx + dy*dy);
-    // // In between r1 and r, we fade the color out as it gets further from the center
-    // float r1 = 0.1;
-
-    // // If we're within the circle, draw a color
-    // if(r < 0.5)
-    //     // gl_FragColor = vColor;
-    //     // Modify the pixel as we get further from the center
-    //     gl_FragColor = 2.0 * (0.5 - r) * vColor;
-
-    // // If we're outside the circle, don't draw anything
-    // if(r >= 0.5)
-    //     gl_FragColor[3] = 0.0;
-    // // If we're within r1 of the circle, draw the pixel at full opacity
-    // else if (r < r1)
-    //     gl_FragColor[3] = vColor[3];
-    // // If we're between r1 and r, set the alpha of this pixel to a proportionate value
-    // else
-    //     gl_FragColor[3] = 1.0 - ((r - r1)/(0.5 - r1))*vColor[3];
-
-    // gl_FragColor = 2.0 * (0.5 - r) * vColor;
     gl_FragColor = vec4(1.0, 0.0, 0.0, 0.5);;
 }
--- shaders/gl-vertex.glsl
+++ shaders/gl-vertex.glsl
@@ -1,55 +1,11 @@
-/*
-* Copyright (C) 2011 Samsung Electronics Corporation. All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided the following conditions
-* are met:
-*
-* 1.  Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*
-* 2.  Redistributions in binary form must reproduce the above copyright
-*     notice, this list of conditions and the following disclaimer in the
-*     documentation and/or other materials provided with the distribution.
-*
-* THIS SOFTWARE IS PROVIDED BY SAMSUNG ELECTRONICS CORPORATION AND ITS
-* CONTRIBUTORS "AS IS", AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING
-* BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SAMSUNG
-* ELECTRONICS CORPORATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES(INCLUDING
-* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-* DATA, OR PROFITS, OR BUSINESS INTERRUPTION), HOWEVER CAUSED AND ON ANY THEORY
-* OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING
-* NEGLIGENCE OR OTHERWISE ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-* EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 precision mediump float;
 uniform mat3 mvp;
 attribute vec2 curPos;
-// varying vec4 vColor;
 
 void main(void) {
     float w = 1.0;
 
-    // float maxSize = 8.0;
-    // float size = maxSize * (1.0 - curPos.z);
-    // if(size < 1.0) size = 1.0;
-    // if(size > maxSize) size = maxSize;
-
-    // gl_PointSize  = size;
     gl_PointSize = 3.0;
-
-    // vec4 color = vec4(1.0, 1.0, 1.0, 1.0);
-    // if(pos.x < 0.0) {
-    //     color[0] = 0.0;
-    // }
-    // if(pos.y < 0.0) {
-    //     color[1] = 0.0;
-    // }
-    // vColor = color;
-    // vColor = vec4(1.0, 0.0, 0.0, 1.0);
 
     vec3 pos = mvp * vec3(curPos[0], curPos[1], w);
     gl_Position = vec4(pos[0], pos[1], 0, pos[2]);
