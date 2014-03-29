e11d7c2ddf29a4d23fe423daee3a5ddb6974649c
Matt Torok <matt@graphistry.com>
2014-03-28 17:22:56 -0700

    Reduce point size from 3 to 2 pixels
    Makes the Uber data look better (otherwise the points just coat the map,) while allowing the other demos to still look good.

--- shaders/point.vertex.glsl
+++ shaders/point.vertex.glsl
@@ -5,7 +5,7 @@ attribute vec2 curPos;
 void main(void) {
     float w = 1.0;
 
-    gl_PointSize = 3.0;
+    gl_PointSize = 2.0;
 
     vec3 pos = mvp * vec3(curPos[0], curPos[1], w);
     gl_Position = vec4(pos[0], pos[1], 0, pos[2]);
