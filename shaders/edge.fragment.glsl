5ca06f23559ff8f687dc103210b5f901ae6e577d
Matt Torok <matt@graphistry.com>
2014-04-03 00:02:24 -0700

    Make edge color more translucent

--- shaders/edge.fragment.glsl
+++ shaders/edge.fragment.glsl
@@ -2,5 +2,6 @@ precision mediump float;
 varying vec4 vColor;
 
 void main(void) {
-    gl_FragColor = vec4(0.0, 0.0, 1.0, 0.65);
+    gl_FragColor = vec4(0.0, 0.0, 1.0, 0.1);
+    // gl_FragColor = vec4(0.5, 0.5, 0.5, 0.1);
 }
