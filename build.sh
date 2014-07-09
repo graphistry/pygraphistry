e0f2a4f24de19c9c4b14d90f3256902156948c1a
Leo Meyerovich <leo@graphistry.com>
2014-07-09 00:05:17 -0700

    Refactor: Port client to browserify from requirejs in preparation for nodecl version.

---
+++ build.sh
@@ -0,0 +1,13 @@
+#### Generates "naivebundle.js", "naivebundle.min.js"
+
+EXTERNAL="-x jQuery" #"-x node-webcl -x node-webgl --ignore fs"
+REQUIRE="-r jQuery"
+OUT="dist/naivebundle"
+VENDORS="dist/vendors"
+ENTRY="js/main.js"
+OUTPACKAGE="Grapher"
+
+
+
+browserify $EXTERNAL -d --standalone $OUTPACKAGE $ENTRY | tee $OUT.js | uglifyjs > $OUT.min.js
+browserify $REQUIRE | tee $VENDORS.js | uglifyjs > $VENDORS.min.js
