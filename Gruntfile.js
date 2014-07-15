54fe7a3ce7ca42b0addc3711f1b9c54014369ced
Matt Torok <github@overblown.net>
2014-07-15 04:49:58 -0700

    streaming of boxes works
    May have to do an `npm install` in the superconductorjs directory.

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+            main: ['src/**/*.js'],
+            options: {
+                jshintrc: '.jshintrc',
+                force: grunt.option('dev')
+            }
+        },
+
+        browserify: {
+            main: {
+                src: ['src/main.js'],
+                dest: 'dist/<%= pkg.name %>.js',
+                options: {
+                    // external: []
+                    bundleOptions: {
+                        debug: grunt.option('dev')
+                    },
+                    watch: grunt.option('dev'),
+                    keepAlive: grunt.option('dev')
+                }
+            }
+        },
+
+        clean: {
+            main: ['dist']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('default', ['jshint', 'browserify']);
+}
