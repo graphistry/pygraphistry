945c8ddacbf61ba33fd8bba2794c68a0c3207238
Matt Torok <github@overblown.net>
2014-07-22 12:49:02 -0700

    WIP: GL distributed rendering

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,12 +22,65 @@ module.exports = function(grunt) {
                     watch: false,
                     keepAlive: false,
                     didRun: false,
+                    external: ['ClientRenderers'],
                     postBundleCB: function(err, src, next) {
                         global['browserifyDidRun'] = true;
                         next(err, src);
+                    },
+                    preBundleCB: function(browserifyInstance) {
+                        // The first (and only first) time this task is run, install an event
+                        // handler on watchify's "update" event. We then limit the files jshint is
+                        // run on to those files.
+                        if(global['browserifyDidSetWatchers']) { return; }
+
+                        browserifyInstance.on('update', function(files) {
+                            grunt.config.set("jshint.main", files);
+                        })
+
+                        global['browserifyDidSetWatchers'] = true;
                     }
                 }
-            }
+            },
+
+            // ClientRenderers: {
+            //     src: ['src/renderer.sc.js'],
+            //     dest: 'dist/ClientRenderers.js',
+            //     options: {
+            //         transform: ['brfs'],
+            //         bundleOptions: {
+            //             debug: true,
+            //             standalone: 'ClientRenderers'
+            //         },
+            //     }
+            // }
+
+            // SCRenderer: {
+            //     src: ['src/renderer.sc.js'],
+            //     dest: 'dist/ClientRenderers.SCRenderer.js',
+            //     options: {
+            //         transform: ['brfs'],
+            //         watch: false,
+            //         keepAlive: false,
+            //         alias: ['src/renderer.sc.js:ClientRenderers'],
+            //         bundleOptions: {
+            //             debug: true,
+            //         },
+            //     }
+            // },
+
+            // GraphRenderer: {
+            //     src: ['src/renderer.graph.js'],
+            //     dest: 'dist/ClientRenderers.Graph.js',
+            //     options: {
+            //         transform: ['brfs'],
+            //         watch: false,
+            //         keepAlive: false,
+            //         alias: ['src/renderer.sc.js:ClientRenderers'],
+            //         bundleOptions: {
+            //             debug: true,
+            //         },
+            //     }
+            // }
         },
 
         exorcise: {
@@ -78,10 +131,12 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-exorcise');
     grunt.loadNpmTasks('grunt-jsdoc');
 
-    grunt.registerTask('default', ['jshint', 'browserify', 'jsdoc', 'exorcise']);
+    grunt.registerTask('default', ['jshint', 'browserify', 'exorcise']);
 
     grunt.registerTask('live', 'Set a global variable.', function() {
         grunt.config.set("browserify.main.options.watch", true);
+        // grunt.config.set("browserify.SCRenderer.options.watch", true);
+        // grunt.config.set("browserify.GraphRenderer.options.watch", true);
         grunt.task.run('default', "watch");
     });
 
