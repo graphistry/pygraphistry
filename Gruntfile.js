0c3c381ca0de6d10468aee7cdd0c9605d77d9e63
Matt Torok <github@overblown.net>
2014-08-06 07:08:58 -0700

    Rename grunt task for SC

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
         },
 
         browserify: {
-            main: {
+            SCMain: {
                 src: ['src/main.sc.js'],
                 dest: 'dist/StreamGL.sc.js',
                 options: {
