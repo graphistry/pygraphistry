91f027f60824060c0211082535ea0e25c4795985
Matt Torok <matt@graphistry.com>
2014-08-19 15:41:07 -0700

    Refactor emit loop in server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-
 "use strict";
+
 
 var Rx = require("rx");
 var _ = require("underscore");
@@ -12,6 +12,16 @@ var express = require("express");
 var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http); // , {transports: ["websocket"]}
+
+
+/** Given an Object with buffers as values, returns the sum size in megabytes of all buffers */
+function vboSizeMB(vbos) {
+    var vboSizeBytes = _.reduce(_.values(vbos.buffers), function(sum, v) {
+            return sum + v.byteLength;
+        }, 0);
+    return Math.round((Math.round(vboSizeBytes / 1024) / 1024) * 100) / 100;
+}
+
 
 
 function nocache(req, res, next) {
@@ -35,30 +45,28 @@ var vboUpdated = driver.create();
 io.on("connection", function(socket) {
     debug("Client connected");
 
-    var emitObs = Rx.Observable.fromCallback(socket.emit, socket);
-    var acknowledged = new Rx.BehaviorSubject(true);
+    var emitFnWrapper = Rx.Observable.fromCallback(socket.emit, socket);
+    var acknowledged = new Rx.BehaviorSubject(0);
 
-    vboUpdated
+    var doEmit = vboUpdated
         .sample(acknowledged)
-        .merge(vboUpdated.take(1))  // Ensure we fire one event to kick off the loop
-        .subscribe(
-            function(vbos) {
-                var vboSizeBytes = _.reduce(_.values(vbos.buffers), function(sum, v) {
-                        return sum + v.byteLength;
-                    }, 0 );
-                var vboSizeMB = Math.round((Math.round(vboSizeBytes / 1024) / 1024) * 100) / 100;
-                debug("Socket", "Emitting VBOs: " + vboSizeMB + "MB");
+        .merge(vboUpdated.take(1));  // Ensure we fire one event to kick off the loop
+    doEmit.subscribe(
+        function(vbos) {
+            debug("Socket", "Emitting VBOs: " + vboSizeMB(vbos.buffers) + "MB");
 
-                emitObs("vbo_update", vbos)
-                    .subscribe(function(clientElapsed) {
-                        debug("Socket", "...client acknowledged. Reported performance: " +
-                            clientElapsed + "ms");
-                        acknowledged.onNext();
-                    },
-                    function(err) { console.err("Error receiving handshake from client:", err); });
-            },
-            function(err) {
-                console.error("Error sending VBO update:", err);
-            }
-        )
+            emitFnWrapper("vbo_update", vbos).subscribe(
+                function(clientElapsed) {
+                    debug("Socket", "...client acknowledged. Reported performance: " +
+                        clientElapsed + "ms");
+                    acknowledged.onNext(clientElapsed);
+                },
+                function(err) { console.err("Error receiving handshake from client:", err); }
+            );
+        },
+        function(err) {
+            console.error("Error sending VBO update:", err);
+        }
+    );
+
 });
