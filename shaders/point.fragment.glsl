9650df64c54d2db98f8bfcfdcca1c315f86eccb6
Leo Meyerovich <leo@graphistry.com>
2014-08-28 12:44:14 -0700

    Use uint8 array for point sizes and do different round circles for node vs webgl

--- shaders/point.fragment.glsl
+++ shaders/point.fragment.glsl
@@ -3,5 +3,5 @@ precision highp float;
 #endif
 
 void main(void) {
-    gl_FragColor = vec4(1.0, 0.3, 0.3, 1.0);
+    gl_FragColor = vec4(1.0, 0.3, 0.3, step(length(gl_PointCoord - 0.5), 0.5));
 }
--- shaders/point.vertex.glsl
+++ shaders/point.vertex.glsl
@@ -7,7 +7,7 @@ attribute float pointSize;
 void main(void) {
     float w = 1.0;
 
-    gl_PointSize = pointSize;
+    gl_PointSize = clamp(pointSize, 0.125, 10.0);
 
     vec3 pos = mvp * vec3(curPos[0], curPos[1], w);
     gl_Position = vec4(pos[0], pos[1], 0.0, pos[2]);
