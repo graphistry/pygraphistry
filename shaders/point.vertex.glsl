32709a3abde5ac77a23ddb32da6755937fbd1744
Leo Meyerovich <leo@graphistry.com>
2014-08-28 00:41:00 -0700

    Optional point sizes and rounding mode

--- shaders/point.vertex.glsl
+++ shaders/point.vertex.glsl
@@ -2,10 +2,12 @@ precision mediump float;
 uniform mat3 mvp;
 attribute vec2 curPos;
 
+attribute float pointSize;
+
 void main(void) {
     float w = 1.0;
 
-    gl_PointSize = 2.0;
+    gl_PointSize = pointSize;
 
     vec3 pos = mvp * vec3(curPos[0], curPos[1], w);
     gl_Position = vec4(pos[0], pos[1], 0.0, pos[2]);
