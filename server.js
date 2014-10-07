eae7caf9c53850ee354aa25d8dc444304b0b20d1
Abe Stanway <abestanway@gmail.com>
2014-10-07 13:43:52 -0400

    Revert server.js config handling

--- server.js
+++ server.js
@@ -4,11 +4,7 @@
 //Set jshint to ignore `predef:'io'` in .jshintrc so we can manually define io here
 /* global -io */
 
-var config      = require('./config');
-
-if (process.argv.length > 2) { 
-    config      = JSON.parse(process.argv[2]);
-}
+var config      = require('./config')(process.argv.length > 2 ? JSON.parse(process.argv[2]) : {});
 
 console.log("Config set to %j", config);
 
