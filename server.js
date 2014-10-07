76c174af54c3a6790e7be20893fac8f5e4beeefa
Abe Stanway <abestanway@gmail.com>
2014-10-07 10:24:32 -0400

    Fix config set

--- server.js
+++ server.js
@@ -4,7 +4,13 @@
 //Set jshint to ignore `predef:'io'` in .jshintrc so we can manually define io here
 /* global -io */
 
-var config      = require('./config')(process.argv.length > 2 ? JSON.parse(process.argv[2]) : {});
+var config      = require('./config');
+
+if (process.argv.length > 2) { 
+    config      = JSON.parse(process.argv[2]);
+}
+
+console.log("Config set to %j", config);
 
 var Rx          = require('rx'),
     _           = require('underscore'),
