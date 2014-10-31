6f626b03efac3e72b495ea1fbce477e94d613d6e
Matt Torok <github@overblown.net>
2014-10-31 16:04:30 -0700

    Add mongolayer credentials to repo
    This is probably bad security practice, but they're already on the
    graphistry repo (as part of the ansible templates,) so this just saves
    us a copy+paste on checkout for dev work.

--- config.js
+++ config.js
@@ -7,7 +7,8 @@ var path = require('path');
 module.exports = function() {
     var defaultOptions = {
         LISTEN_ADDRESS: '0.0.0.0',
-        LISTEN_PORT: 10000
+        LISTEN_PORT: 10000,
+        MONGO_SERVER: 'mongodb://graphistry:graphtheplanet@lighthouse.2.mongolayer.com:10048,lighthouse.3.mongolayer.com:10048/graphistry-prod'
     };
 
     var commandLineOptions = process.argv.length > 2 ? JSON.parse(process.argv[2]) : {};
