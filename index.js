29b7438f85aad17f85382be134b198a275f71c80
Matt Torok <github@overblown.net>
2014-10-31 17:31:45 -0700

    Remove STATIC_HTTP_PATH exported field
    This is now supplied by nodecl.

--- index.js
+++ index.js
@@ -8,7 +8,3 @@ exports.render_config = {
     'graph': require('./src/renderer.config.graph'),
     'superconductor': require('./src/renderer.config.sc')
 };
-
-// FIXME: Absolute path to static files (HTML/CSS/JS) that should be served by the HTTP server
-// (This is a temporary hack. Should point to the `superconductor2/nodecl/GPUSteaming` directory.)
-exports.STATIC_HTTP_PATH = path.resolve(__dirname, '../');
