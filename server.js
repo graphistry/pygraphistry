11f4b5f6f9ec68a87ef76c45a599274066134432
Leo Meyerovich <leo@graphistry.com>
2014-11-03 19:21:17 -0800

    Make graph-viz work via streamgl include and horizon via static hosting (FIXME)

--- server.js
+++ server.js
@@ -23,15 +23,29 @@ app.get('/vizaddr/graph', function(req, res) {
     res.json({'hostname': VIZ_SERVER_HOST, 'port': VIZ_SERVER_PORT});
 });
 
+app.get('/vizaddr/horizon', function(req, res) {
+    debug("Assigning client '%s' to viz server on %s, port %d", req.ip, VIZ_SERVER_HOST, VIZ_SERVER_PORT);
+    res.json({'hostname': VIZ_SERVER_HOST, 'port': VIZ_SERVER_PORT});
+});
+
 
 // Serve the socket.io client library
 app.get('*/socket.io.js', function(req, res) {
     res.sendFile(require.resolve('socket.io-client/socket.io.js'));
 });
+var path = require('path');
+app.get('*/StreamGL.js', function(req, res) {
+    res.sendFile(path.resolve(__dirname, 'node_modules/StreamGL/dist/StreamGL.js'));
+});
 app.use(express.static(STATIC_FILE_PATH));
+
+//horizon
+app.use('/horizon', express.static('/Users/lmeyerov/Desktop/Superconductor2'));
+
 
 // Default '/' path redirects to graph demo
 app.get('/', function(req, res) {
+    debug('redirecting')
     res.redirect('/graph.html' + (req.query.debug !== undefined ? '?debug' : ''));
 });
 
