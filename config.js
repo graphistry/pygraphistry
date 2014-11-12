effbb55e3728d94c77300fbe1bf196bd6a29c7d0
Abe Stanway <abestanway@gmail.com>
2014-11-12 16:23:19 -0500

    Metrics in node-driver

--- config.js
+++ config.js
@@ -7,7 +7,8 @@ module.exports = function() {
     var defaultOptions = {
         LISTEN_ADDRESS: '0.0.0.0',
         LISTEN_PORT: 10000,
-        MONGO_SERVER: 'mongodb://graphistry:graphtheplanet@lighthouse.2.mongolayer.com:10048,lighthouse.3.mongolayer.com:10048/graphistry-prod'
+        MONGO_SERVER: 'mongodb://graphistry:graphtheplanet@lighthouse.2.mongolayer.com:10048,lighthouse.3.mongolayer.com:10048/graphistry-prod',
+        HOSTNAME: 'localhost'
     };
 
     var commandLineOptions = process.argv.length > 2 ? JSON.parse(process.argv[2]) : {};
