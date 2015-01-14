6a7b0484396102eff89d4291e70224905226cc55
Quinn Johnson <quinn@Quinns-MacBook-Pro.local>
2015-01-14 14:46:54 -0800

    Updated driverSpec test to use new dataset loading API

--- spec/node-driver/driverSpec.js
+++ spec/node-driver/driverSpec.js
@@ -10,37 +10,50 @@ var renderConfig = require('../../js/renderer.config.js').scenes.default;
 var loader       = require('../../js/data-loader.js');
 
 describe("Smoke test for server loop", function() {
-    var theDataset = loader.downloadDataset('Uber');
-    var activeBuffers = renderer.getServerBufferNames(renderConfig),
+    var theDataset;
+    var activeBuffers;
+    var activeBuffers;
+    var ticks;
+
+    beforeEach(function() {
+        theDataset = loader.downloadDataset('Uber');
+        activeBuffers = renderer.getServerBufferNames(renderConfig)
         activePrograms = renderConfig.render;
-    var anim = driver.create(theDataset);
+        ticks = theDataset.then(function(dataset){
+            return driver.create(dataset).ticks;
+        });
+    });
 
     it("Setup", function (done) {
         var fail = this.fail;
 
-        var tick = anim.ticks.take(1);
-        tick.subscribe(function (e) {
-            expect(e).not.toBeNull();
-            expect(e).toBeDefined();
-            done();
-        }, fail);
+        ticks.then( function (ticks) {
+            var tick = ticks.take(1);
+            tick.subscribe(function (e) {
+                expect(e).not.toBeNull();
+                expect(e).toBeDefined();
+                done();
+            }, fail);
+        });
     });
 
-    var graph = new Rx.ReplaySubject(1);
-    var ticksMulti = anim.ticks.publish();
-    ticksMulti.connect();
-    ticksMulti.take(1).subscribe(graph);
-
     it("Fetch VBO", function (done) {
-        var fail = this.fail;
+        ticks.then(function (ticks) {
+            var graph = new Rx.ReplaySubject(1);
+            var ticksMulti = ticks.publish();
+            ticksMulti.connect();
+            ticksMulti.take(1).subscribe(graph);
 
-        graph.flatMap(function (graph) {
-            return driver.fetchData(graph, renderConfig, compress, activeBuffers, undefined, activePrograms); 
-        }).take(1).subscribe(function (vbo) {
-            expect(vbo).not.toBeNull();
-            expect(vbo).toBeDefined();
-            done();
-        }, fail);
+            var fail = this.fail;
+
+            graph.flatMap(function (graph) {
+                return driver.fetchData(graph, renderConfig, compress, activeBuffers, undefined, activePrograms);
+            }).take(1).subscribe(function (vbo) {
+                expect(vbo).not.toBeNull();
+                expect(vbo).toBeDefined();
+                done();
+            }, fail);
+        });
     });
 });
 
