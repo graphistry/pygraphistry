89599a904bbf22d3cc32a47384fe15f52c9dcde5
Quinn Johnson <quinn@Quinns-MacBook-Pro.local>
2015-01-21 17:42:43 -0800

    Updated deployment scripts to check if tests passed.

--- deploy/prod-deploy.sh
+++ deploy/prod-deploy.sh
@@ -8,16 +8,17 @@ then
 fi
 
 chmod 600 ansible_id_rsa.pem
-echo "Checking that all local repos are up-to-date..."
+echo "Checking that all local repos are up-to-date and passing tests..."
 OUTPUT=`./check.sh`
 PULLCOUNT=`echo $OUTPUT | grep "Need to pull" | wc -l`
 PUSHCOUNT=`echo $OUTPUT | grep "Need to push" | wc -l`
 DIVCOUNT=`echo $OUTPUT | grep "Diverged" | wc -l`
 COMMITCOUNT=`echo $OUTPUT | grep "local changes" | wc -l`
-COUNT=$(($PULLCOUNT + $PUSHCOUNT + $DIVCOUNT + $COMMITCOUNT))
+FAILURECOUNT=`echo $OUTPUT | grep "Tests FAILED" | wc -l`
+COUNT=$(($PULLCOUNT + $PUSHCOUNT + $DIVCOUNT + $COMMITCOUNT + $FAILURECOUNT))
 
 if [ $COUNT = "0" ] ; then
-    echo "All repos up-to-date, deploying production..."
+    echo "All repos up-to-date, all tests passed. Deploying production..."
     ansible-playbook system.yml -vv --tags fast --skip-tags provision,staging-slack -i hosts -l prod
 else
     echo "$OUTPUT"
--- deploy/stage-deploy.sh
+++ deploy/stage-deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 chmod 600 ansible_id_rsa.pem
-echo "Checking that all local repos are up-to-date..."
+echo "Checking that all local repos are up-to-date and passing tests..."
 OUTPUT=`./check.sh`
 PULLCOUNT=`echo $OUTPUT | grep "Need to pull" | wc -l`
 PUSHCOUNT=`echo $OUTPUT | grep "Need to push" | wc -l`
 DIVCOUNT=`echo $OUTPUT | grep "Diverged" | wc -l`
 COMMITCOUNT=`echo $OUTPUT | grep "local changes" | wc -l`
-COUNT=$(($PULLCOUNT + $PUSHCOUNT + $DIVCOUNT + $COMMITCOUNT))
+FAILURECOUNT=`echo $OUTPUT | grep "Tests FAILED" | wc -l`
+COUNT=$(($PULLCOUNT + $PUSHCOUNT + $DIVCOUNT + $COMMITCOUNT + $FAILURECOUNT))
 
 if [ $COUNT = "0" ] ; then
-    echo "All repos up-to-date, deploying staging..."
+    echo "All repos up-to-date, all tests passed. Deploying staging..."
     ansible-playbook system.yml -vv --tags fast --skip-tags provision,prod-slack -i hosts -l staging
 else
     echo "$OUTPUT"
