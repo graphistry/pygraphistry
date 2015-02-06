24993b1325deab5189ef7dfa64b94d20d2c0584e
Leo Meyerovich <leo@graphistry.com>
2015-02-06 04:12:00 -0800

    fix frag color order

--- shaders/point.fragment.glsl
+++ shaders/point.fragment.glsl
@@ -5,5 +5,5 @@ precision highp float;
 varying vec4 vColor;
 
 void main(void) {
-    gl_FragColor = vec4(vColor.y, vColor.z, vColor.w, vColor.x * step(length(gl_PointCoord - 0.5), 0.5));
+    gl_FragColor = vec4(vColor.x, vColor.y, vColor.z, vColor.w * step(length(gl_PointCoord - 0.5), 0.5));
 }
