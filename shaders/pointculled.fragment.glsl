bb752d724b08ce3672c9da85e7ec651950280f83
Leo Meyerovich <leo@graphistry.com>
2015-02-23 23:31:07 -0800

    Smoother outline

--- shaders/pointculled.fragment.glsl
+++ shaders/pointculled.fragment.glsl
@@ -6,8 +6,8 @@ uniform float stroke;
 
 void main(void) {
 
-    float radius = length(gl_PointCoord - 0.5);
-    float alpha = vColor.w * step(radius, 0.5);
-    vec4 fillColor = stroke > 0.0 ? vec4(vec3(vColor)*0.5,alpha) : vec4(vColor.x, vColor.y, vColor.z, alpha);
+    float radius = length(gl_PointCoord * 2.0 - vec2(1.0));
+    float alpha = smoothstep(1.0, 0.95, radius);
+    vec4 fillColor = stroke > 0.0 ? vec4(vec3(vColor)*0.5,alpha) : vec4(vColor.x, vColor.y, vColor.z, vColor.w * alpha);
     gl_FragColor = fog > 1.0 ? fillColor : vColor;
 }
