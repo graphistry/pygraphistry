8e8be5b9135a35299de471240bf3485ffbc79b63
Paden Tomasello <pdtomasello@gmail.com>
2015-04-13 15:13:18 -0700

    Merge in files from master

--- shaders/midedgeculled.vertex.glsl
+++ shaders/midedgeculled.vertex.glsl
@@ -13,13 +13,10 @@ varying vec4 eColor;
 
 void main(void) {
     vec4 pos = mvp * vec4(curPos.x, 1.0 * curPos.y, Z_VAL, W_VAL);
-
     float furthestComponent = max(abs(pos.x), abs(pos.y));
-    float m = 1.0 / (1.02 - 1.05);
-    float b = -1.0 * m * 1.05;
-    float remapped = m * furthestComponent + b;
+    float remapped = (-furthestComponent + SENSITIVITY) / SENSITIVITY;
 
     alpha = clamp(remapped, 0.0, 1.0);
-    gl_Position = pos;
     eColor = edgeColor;
+    gl_Position = pos;
 }
