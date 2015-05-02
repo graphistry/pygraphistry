2aba743e305a465f7035a8590fb706d9096516ef
thibaudh <thibaud@graphistry.com>
2015-05-01 21:24:56 -0700

    Hide arrows for small edges. Arrow size depends on edge size

--- shaders/arrow.vertex.glsl
+++ shaders/arrow.vertex.glsl
@@ -6,36 +6,55 @@ precision highp float;
 uniform mat4 mvp;
 uniform float zoomScalingFactor;
 uniform float maxPointSize;
-uniform float screenWidth;
-uniform float screenHeight;
+uniform float maxScreenSize;
+uniform float maxCanvasSize;
 
-attribute vec2 curPos;
+attribute vec2 startPos;     // Start position of edge
+attribute vec2 endPos;       // End position of edge
+attribute float normalDir;   // Direction of normal vector.
+                             // 0 for tip vertex Wing vertices -> 1.0/-1.0
 attribute float pointSize;
 attribute vec4 arrowColor;
-attribute vec2 edgeVec;
 
 varying vec4 aColor;
 
-void main(void) {
-    float radius = clamp(zoomScalingFactor * pointSize, 7.0, maxPointSize) / 2.0;
-    vec4 offset = mvp * vec4(edgeVec.xy, Z_VAL, 0.0);
-    vec4 noffset = normalize(offset);
-    
-    vec4 pos0 = mvp * vec4(curPos.xy, Z_VAL, W_VAL);
-    //vec4 pos = pos0;
-
-    //pos.x = pos0.x + (radius / screenWidth) * noffset.x;
-    //pos.y = pos0.y + (radius / screenHeight) * noffset.y;
-    //
-    vec4 pos = pos0 + noffset / 100.0;
-
-    float furthestComponent = max(abs(pos0.x), abs(pos0.y));
+void culledAlpha(in vec2 pos, out float alpha) {
+    float furthestComponent = max(abs(pos.x), abs(pos.y));
     float m = 1.0 / (1.02 - 1.05);
     float b = -1.0 * m * 1.05;
     float remapped = m * furthestComponent + b;
-    float alpha = clamp(remapped, 0.0, 1.0);
+    alpha = clamp(remapped, 0.0, 1.0);
+}
 
-    pos.z = 1.0 - alpha;
+void main(void) {
+    vec2 arrow = startPos - endPos;
+    vec4 edgeVec = vec4(arrow.xy, Z_VAL, 0.0);
+    vec4 edgeVecN = normalize(edgeVec);
+    vec4 normalVecN = vec4(-edgeVecN.y, edgeVecN.x, edgeVecN.z, edgeVecN.w);
+    vec4 screenEdgeVec = mvp * edgeVec;
+    float edgeLength = length(screenEdgeVec);
+
+    vec4 pointPos = vec4(endPos.xy, Z_VAL, W_VAL);
+
+    vec4 offset = abs(normalDir) * edgeVecN + 0.5 * normalDir * normalVecN;
+    float semanticSizeFactor = clamp(-75.0 * edgeLength + 135.0, 60.0, 120.0);
+    //float semanticSizeFactor = clamp(-100.0 * edgeLength + 170.0, 70.0, 160.0);
+    float arrowSize = maxScreenSize / semanticSizeFactor;
+    vec4 pos0 = pointPos + arrowSize * offset;
+
+    // Displace arrow to move the tip from the center to the edge of the point.
+    float pointRadius = clamp(zoomScalingFactor * pointSize, 7.0, maxPointSize);
+    vec4 displacement = normalize(screenEdgeVec) * pointRadius / maxCanvasSize;
+    vec4 pos = mvp * pos0 + displacement;
+
+    // Cull arrow if its point is offscreen.
+    vec4 screenPointPos = mvp * pointPos;
+    float alpha;
+    culledAlpha(screenPointPos.xy, alpha);
+
+    // Clip small arrows by placing them behing the far plane.
+    pos.z = edgeLength < 0.2 ? 100.0 : 1.0 - alpha;
+
     gl_Position = pos;
     aColor = vec4(arrowColor.xyz, alpha);
 }
