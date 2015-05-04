95a0581e24d768fde547566cbd5c01a02d46728e
Quinn Johnson <quinn@graphistry.com>
2015-05-04 13:07:53 -0700

    Added shaders for edge highlight

---
+++ shaders/edgehighlighted.vertex.glsl
@@ -0,0 +1,29 @@
+precision mediump float;
+
+#define W_VAL 1.0
+#define Z_VAL 0.1
+
+uniform mat4 mvp;
+
+attribute vec2 curPos;
+
+varying vec4 eColor;
+
+void main(void) {
+    vec4 pos = mvp * vec4(curPos.x, curPos.y, Z_VAL, W_VAL);
+
+    float furthestComponent = max(abs(pos.x), abs(pos.y));
+    float m = 1.0 / (1.02 - 1.05);
+    float b = -1.0 * m * 1.05;
+    float remapped = m * furthestComponent + b;
+    float alpha = clamp(remapped, 0.0, 1.0);
+    if (alpha > 0.0) {
+        alpha = 1.0;
+    }
+
+    pos.z = 1.0 - alpha;
+    gl_Position = pos;
+
+    // Should be orange
+    eColor =  vec4(0.89, 0.369, 0.0745, alpha);
+}
