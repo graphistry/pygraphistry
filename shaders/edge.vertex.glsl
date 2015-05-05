e5ff6d82dda3773b644ce25298c0bf7969c7180c
Quinn Johnson <quinn@graphistry.com>
2015-05-05 16:56:11 -0700

    Fixed opacity issues on edgepicking shaders

--- shaders/edge.vertex.glsl
+++ shaders/edge.vertex.glsl
@@ -8,7 +8,7 @@ attribute vec2 curPos;
 
 attribute vec4 edgeColor;
 varying vec4 eColor;
-varying float alpha;
+varying vec3 eColorFullOpacity;
 
 void main(void) {
     vec4 pos = mvp * vec4(curPos.x, curPos.y, Z_VAL, W_VAL);
@@ -17,10 +17,12 @@ void main(void) {
     float furthestComponent = max(abs(pos.x), abs(pos.y));
     float m = 1.0 / (1.02 - 1.05);
     float b = -1.0 * m * 1.05;
-    alpha = m * furthestComponent + b;
+    float remapped = m * furthestComponent + b;
+    float alpha = clamp(remapped, 0.0, 1.0);
 
     gl_Position = pos;
 
     // eColor = edgeColor;
-    eColor = vec4(edgeColor.g, edgeColor.b, edgeColor.a, 1.0);
+    eColor = vec4(edgeColor.g, edgeColor.b, edgeColor.a, alpha);
+    eColorFullOpacity = vec3(edgeColor.g, edgeColor.b, edgeColor.a);
 }
