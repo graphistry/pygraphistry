c47918d79902aa04e1da918bb45f53464e776ebf
Quinn Johnson <quinn@graphistry.com>
2015-05-11 12:45:21 -0700

    Fixed flipping issue on safari with fullscreen texture

--- shaders/fullscreen.vertex.glsl
+++ shaders/fullscreen.vertex.glsl
@@ -3,6 +3,8 @@ precision mediump float;
 // Vertex Position in normalized range ([-1,+1])
 attribute vec2 vertexPosition;
 
+uniform float flipTexture;
+
 varying vec2 textureCoords;
 
 const vec2 scale = vec2(0.5, 0.5);
@@ -11,8 +13,10 @@ void main(void) {
     // scale vertex attribute to [0,1] range
     textureCoords = vertexPosition * scale + scale;
 
-    // Flip Y coordinate
-    textureCoords.y = 1.0 - textureCoords.y;
+    // Flip Y coordinate if uniform says to
+    if (flipTexture > 0.0) {
+        textureCoords.y = 1.0 - textureCoords.y;
+    }
 
     gl_Position = vec4(vertexPosition, 1.0, 1.0);
 }
