39d4f4c1be8aaba81ebb2944027dcdc5ad50d1fd
Paden Tomasello <pdtomasello@gmail.com>
2015-06-10 15:12:19 -0700

    Change midpoint rendering to match edges

--- shaders/midedgeculled.fragment.glsl
+++ shaders/midedgeculled.fragment.glsl
@@ -2,9 +2,8 @@
 precision highp float;
 #endif
 
-varying float alpha;
 varying vec4 eColor;
 
 void main(void) {
-    gl_FragColor = vec4(eColor.x, eColor.y, eColor.z, alpha);
+    gl_FragColor = eColor;
 }
--- shaders/midedgeculled.vertex.glsl
+++ shaders/midedgeculled.vertex.glsl
@@ -3,10 +3,11 @@ precision mediump float;
 #define W_VAL 1.0
 #define Z_VAL 0.0
 #define SENSITIVITY 0.99
+#define M -33.33 // 1.0 / (1.02 - 1.05);
+#define B 35.0
 
 uniform mat4 mvp;
 attribute vec2 curPos;
-varying float alpha;
 
 attribute vec4 edgeColor;
 varying vec4 eColor;
@@ -14,10 +15,11 @@ varying vec4 eColor;
 void main(void) {
     vec4 pos = mvp * vec4(curPos.x, 1.0 * curPos.y, Z_VAL, W_VAL);
     float furthestComponent = max(abs(pos.x), abs(pos.y));
-    float remapped = (-furthestComponent + SENSITIVITY) / SENSITIVITY;
+    float remapped = M * furthestComponent + B;
 
-    alpha = clamp(remapped, 0.0, 0.1);
-    eColor = edgeColor;
-    eColor = edgeColor;
+    float alpha = clamp(remapped, 0.0, 1.0);
+    pos.z = 1.0 - alpha;
+
+    eColor = vec4(edgeColor.xyz, alpha);
     gl_Position = pos;
 }
