1f4dd48e0bc8e2a6ad936468160fdb08f50be4fa
thibaudh <thibaud@graphistry.com>
2015-08-09 16:40:37 -0700

    Fix and update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python
 
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
-
 from setuptools import setup, find_packages
 #from codecs import open
 #from os import path
@@ -17,36 +11,34 @@ from setuptools import setup, find_packages
 
 setup(
     name='graphistry',
-
     version='0.0.22',
     packages = ['graphistry'],
+    platforms='any',
     description = 'Visualize node-link graphs using Graphistry\'s cloud',
     #long_description=long_description,
-
     url='https://github.com/graphistry/pygraphistry',
-
-    author='Graphistry',
+    download_url= 'https://pypi.python.org/pypi/graphistry/'
+    author='The Graphistry Team',
     author_email='pygraphistry@graphistry.com',
     setup_requires=['numpy'],
     install_requires=['numpy', 'pandas', 'requests', 'future'],
-
+    extras_require={
+        'igraph': ['python-igraph'],
+        'all': ['python-igraph', 'numexpr', 'Bottleneck']
+    },
     license='BSD',
-
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Scientific/Engineering :: Information Analysis',
     ],
-
-    keywords='graph plot visualization pandas igraph',
-
-    packages=find_packages(exclude=['contrib', 'tests*'])
+    keywords=['Graph', 'Network', 'Plot', 'Visualization', 'Pandas', 'Igraph']
 )
