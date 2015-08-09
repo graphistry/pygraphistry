43ba4e458bfee531789973ceda1a404d1db41b31
thibaudh <thibaud@graphistry.com>
2015-08-08 19:27:37 -0700

    Privatize plotter attributes

--- graphistry/plotter.py
+++ graphistry/plotter.py
@@ -17,40 +17,40 @@ class Plotter(object):
 
     def __init__(self):
         # Bindings
-        self.edges = None
-        self.nodes = None
-        self.source = None
-        self.destination = None
-        self.node = None
-        self.edge_title = None
-        self.edge_label = None
-        self.edge_color = None
-        self.edge_weight = None
-        self.point_title = None
-        self.point_label = None
-        self.point_color = None
-        self.point_size = None
+        self._edges = None
+        self._nodes = None
+        self._source = None
+        self._destination = None
+        self._node = None
+        self._edge_title = None
+        self._edge_label = None
+        self._edge_color = None
+        self._edge_weight = None
+        self._point_title = None
+        self._point_label = None
+        self._point_color = None
+        self._point_size = None
         # Settings
-        self.height = 500
-        self.url_params = {'info': 'true'}
+        self._height = 500
+        self._url_params = {'info': 'true'}
 
     def bind(self, source=None, destination=None, node=None,
              edge_title=None, edge_label=None, edge_color=None, edge_weight=None,
              point_title=None, point_label=None, point_color=None, point_size=None):
         res = copy.copy(self)
-        res.source = source or self.source
-        res.destination = destination or self.destination
-        res.node = node or self.node
+        res.source = source or self._source
+        res.destination = destination or self._destination
+        res.node = node or self._node
 
-        res.edge_title = edge_title or self.edge_title
-        res.edge_label = edge_label or self.edge_label
-        res.edge_color = edge_color or self.edge_color
-        res.edge_weight = edge_weight or self.edge_weight
+        res.edge_title = edge_title or self._edge_title
+        res.edge_label = edge_label or self._edge_label
+        res.edge_color = edge_color or self._edge_color
+        res.edge_weight = edge_weight or self._edge_weight
 
-        res.point_title = point_title or self.point_title
-        res.point_label = point_label or self.point_label
-        res.point_color = point_color or self.point_color
-        res.point_size = point_size or self.point_size
+        res.point_title = point_title or self._point_title
+        res.point_label = point_label or self._point_label
+        res.point_color = point_color or self._point_color
+        res.point_size = point_size or self._point_size
         return res
 
     def nodes(nodes):
@@ -71,22 +71,22 @@ class Plotter(object):
 
     def settings(self, height=None, url_params={}):
         res = copy.copy(self)
-        res.height = height or self.height
-        res.url_params = dict(self.url_params, **url_params)
+        res.height = height or self._height
+        res.url_params = dict(self._url_params, **url_params)
         return res
 
     def plot(self, graph=None, nodes=None):
         if graph is None:
-            if self.edges is None:
+            if self._edges is None:
                 util.error('Graph/edges must be specified.')
-            g = self.edges
+            g = self._edges
         else:
             g = graph
-        n = self.nodes if nodes is None else nodes
+        n = self._nodes if nodes is None else nodes
 
-        if self.source is None or self.destination is None:
+        if self._source is None or self._destination is None:
             util.error('Source/destination must be bound before plotting.')
-        if n is not None and self.node is None:
+        if n is not None and self._node is None:
             util.error('Node identifier must be bound when using node dataframe.')
         dataset = self._plot_dispatch(g, n)
         if dataset is None:
@@ -94,7 +94,7 @@ class Plotter(object):
 
         json_dataset = json.dumps(dataset, ensure_ascii=False).encode('utf8')
         dataset_name = pygraphistry.PyGraphistry._etl(json_dataset)
-        viz_url = pygraphistry.PyGraphistry._viz_url(dataset_name, self.url_params)
+        viz_url = pygraphistry.PyGraphistry._viz_url(dataset_name, self._url_params)
 
         if util.in_ipython() is True:
             from IPython.core.display import HTML
@@ -105,29 +105,29 @@ class Plotter(object):
             webbrowser.open(viz_url)
             return self
 
-    def pandas2igraph(self, edges):
+    def pandas2igraph(self, edges, directed=True):
         import igraph
         eattribs = edges.columns.values.tolist()
-        eattribs.remove(self.source)
-        eattribs.remove(self.destination)
-        cols = [self.source, self.destination] + eattribs
+        eattribs.remove(self._source)
+        eattribs.remove(self._destination)
+        cols = [self._source, self._destination] + eattribs
         etuples = [tuple(x) for x in edges[cols].values]
-        self.node = self.node or Plotter._defaultNodeId
-        return igraph.Graph.TupleList(etuples, directed=True, edge_attrs=eattribs,
-                                      vertex_name_attr=self.node)
+        self._node = self._node or Plotter._defaultNodeId
+        return igraph.Graph.TupleList(etuples, directed=directed, edge_attrs=eattribs,
+                                      vertex_name_attr=self._node)
 
     def igraph2pandas(self, ig):
         def get_edgelist(ig):
-            idmap = dict(enumerate(ig.vs[self.node]))
+            idmap = dict(enumerate(ig.vs[self._node]))
             for e in ig.es:
                 t = e.tuple
-                yield dict({self.source: idmap[t[0]], self.destination: idmap[t[1]]},
+                yield dict({self._source: idmap[t[0]], self._destination: idmap[t[1]]},
                             **e.attributes())
 
         edata = get_edgelist(ig)
         ndata = [v.attributes() for v in ig.vs]
         nodes = pandas.DataFrame(ndata, columns=ig.vs.attributes())
-        cols = [self.source, self.destination] + ig.es.attributes()
+        cols = [self._source, self._destination] + ig.es.attributes()
         edges = pandas.DataFrame(edata, columns=cols)
         return (edges, nodes)
 
@@ -156,7 +156,7 @@ class Plotter(object):
             elif default:
                 df[pbname] = df[default]
 
-        nodeid = self.node or Plotter._defaultNodeId
+        nodeid = self._node or Plotter._defaultNodeId
         elist = edges.reset_index()
         bind(elist, 'edgeColor', 'edge_color')
         bind(elist, 'edgeLabel', 'edge_label')
@@ -164,7 +164,7 @@ class Plotter(object):
         bind(elist, 'edgeWeight', 'edge_weight')
         if nodes is None:
             nodes = pandas.DataFrame()
-            nodes[nodeid] = pandas.concat([edges[self.source], edges[self.destination]],
+            nodes[nodeid] = pandas.concat([edges[self._source], edges[self._destination]],
                                            ignore_index=True).drop_duplicates()
 
         nlist = nodes.reset_index()
@@ -178,8 +178,8 @@ class Plotter(object):
         edict = elist.where((pandas.notnull(elist)), None).to_dict(orient='records')
         name = ''.join(random.choice(string.ascii_uppercase +
                                      string.digits) for _ in range(10))
-        bindings = {'idField': self.node or Plotter._defaultNodeId,
-                    'destinationField': self.destination, 'sourceField': self.source}
+        bindings = {'idField': self._node or Plotter._defaultNodeId,
+                    'destinationField': self._destination, 'sourceField': self._source}
         dataset = {'name': pygraphistry.PyGraphistry._dataset_prefix + name,
                    'bindings': bindings, 'type': 'edgelist', 'graph': edict}
         if nlist is not None:
@@ -189,4 +189,4 @@ class Plotter(object):
 
     def _iframe(self, url):
         tag = '<iframe src="%s" style="width:100%%; height:%dpx; border: 1px solid #DDD"></iframe>'
-        return tag % (url, self.height)
+        return tag % (url, self._height)
