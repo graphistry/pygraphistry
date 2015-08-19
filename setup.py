577c3f6a0afdc1640bf73635843affee5b0e23d5
thibaudh <thibaud@graphistry.com>
2015-08-19 10:53:33 -0700

    Version 0.9.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='graphistry',
-    version='0.9.2',
+    version='0.9.3',
     packages = ['graphistry'],
     platforms='any',
     description = 'Visualize node-link graphs using Graphistry\'s cloud',
