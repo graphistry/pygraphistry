ce6b0bc8fcfa7aa0a08eb7929da3cc0fe8e4502f
Leo Meyerovich <leo@graphistry.com>
2015-08-21 23:43:43 -0700

    Exclude some docs and link source

--- docs/build.sh
+++ docs/build.sh
@@ -1,3 +1,5 @@
-sphinx-apidoc -o source ..
+EXCLUDES="../setup.py ../graphistry/util.py"
+echo "SKIPPING " $EXCLUDES
+sphinx-apidoc -o source .. $EXCLUDES
 make html
 open build/html/graphistry.html
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath('../..'))
+
 # -*- coding: utf-8 -*-
 #
 # PyGraphistry documentation build configuration file, created by
@@ -29,7 +33,11 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.ifconfig',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
