890afad437b5f31fc276d47cb85cbe93c9d26d13
thibaudh <thibaud@graphistry.com>
2015-08-23 16:53:47 -0700

    Provide a long_description for PiP homepage which links to github

--- setup.py
+++ setup.py
@@ -2,6 +2,35 @@
 
 from setuptools import setup, find_packages
 
+long_description = """
+**PyGraphistry** is a visual graph analytics library to extract, transform, and
+load big graphs into `Graphistry's<http://www.graphistry.com>`_ GPU-cloud-accelerated
+explorer.
+
+PyGraphistry is...
+
+- **Fast & Gorgeous**: Cluster, filter, and inspect large amounts of data at
+  interactive speed. We layout graphs with a descendant of the gorgeous
+  ForceAtlas2 layout algorithm introduced in Gephi. Our data explorer connects
+  to Graphistry's GPU cluster to layout and render hundreds of thousand of
+  nodes+edges in your browser at unparalleled speeds.
+
+- **Notebook Friendly**: PyGraphistry plays well with interactive notebooks
+  like IPython/Juypter, Zeppelin, and Databricks: Process, visualize, and drill
+  into with graphs directly within your notebooks.
+
+- **Batteries Included**: PyGraphistry works out-of-the-box with popular data
+  science and graph analytics libraries. It is also very easy to use. To create
+  the visualization shown above, download this dataset of Facebook communities
+  from SNAP and load it with your favorite library
+
+
+Try It Out!
+-----------
+
+Tutorial and API docs are on
+`https://github.com/graphistry/pygraphistry<https://github.com/graphistry/pygraphistry>`_
+"""
 
 setup(
     name='graphistry',
@@ -9,7 +38,7 @@ setup(
     packages = ['graphistry'],
     platforms='any',
     description = 'Visualize node-link graphs using Graphistry\'s cloud',
-    #long_description=long_description,
+    long_description=long_description,
     url='https://github.com/graphistry/pygraphistry',
     download_url= 'https://pypi.python.org/pypi/graphistry/',
     author='The Graphistry Team',
