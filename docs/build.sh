05f27f4cf0360d0ae8f68632464e761244005b52
thibaudh <thibaud@graphistry.com>
2015-08-23 12:58:14 -0700

    docs/build.sh: Added Shebang and fix Mac only open

--- docs/build.sh
+++ docs/build.sh
@@ -1,5 +1,11 @@
+#!/bin/sh
+
 EXCLUDES="../setup.py ../graphistry/util.py"
 echo "SKIPPING " $EXCLUDES
 sphinx-apidoc -o source .. $EXCLUDES
 make html
-open build/html/graphistry.html
+
+PLATFORM=`uname`
+if [ "$PLATFORM" == "Darwin" ]; then
+    open build/html/graphistry.html
+fi
