d8be16b5e021ee9759a838828b9a65cc07041428
Leo Meyerovich <leo@graphistry.com>
2015-08-30 10:32:39 -0700

    Revert "Midedge culling: update mid edge shader to use seg's start/end for alpha, and provide phantom buffers in renderconfig"
    This reverts commit 5644f7688db5c044bde46cb64fee6004d244ab7c.

--- shaders/midedgeculled.vertex.glsl
+++ shaders/midedgeculled.vertex.glsl
@@ -8,8 +8,6 @@ precision mediump float;
 
 uniform mat4 mvp;
 attribute vec2 curPos;
-attribute vec2 startPos;
-attribute vec2 endPos;
 
 attribute vec4 edgeColor;
 varying vec4 eColor;
@@ -18,16 +16,10 @@ uniform float edgeOpacity;
 
 void main(void) {
     vec4 pos = mvp * vec4(curPos.x, 1.0 * curPos.y, Z_VAL, W_VAL);
+    float furthestComponent = max(abs(pos.x), abs(pos.y));
+    float remapped = M * furthestComponent + B;
 
-    vec4 sPos = mvp * vec4(startPos.x, startPos.y, Z_VAL, W_VAL);
-    float furthestS = max(abs(sPos.x), abs(sPos.y));
-    float remappedS = clamp(M * furthestS + B, 0.0, 1.0);
-
-    vec4 ePos = mvp * vec4(endPos.x, endPos.y, Z_VAL, W_VAL);
-    float furthestE = max(abs(ePos.x), abs(ePos.y));
-    float remappedE = clamp(M * furthestE + B, 0.0, 1.0);
-
-    float alpha = (remappedS + remappedE) / 2.0;
+    float alpha = clamp(remapped, 0.0, 1.0);
     pos.z = 1.0 - alpha;
 
     eColor = vec4(edgeColor.xyz, edgeOpacity * alpha);
