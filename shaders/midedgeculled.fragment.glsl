0cf47f43b32d94974e6fa9327d92618d1d6d1a4e
Leo Meyerovich <leo@graphistry.com>
2015-08-30 18:54:44 -0700

    Opt: no longer need fancy frag shader

--- shaders/midedgeculled.fragment.glsl
+++ shaders/midedgeculled.fragment.glsl
@@ -1,14 +1,9 @@
 precision mediump float;
 
 varying vec4 eColor;
-uniform float isOpaque; //map non-transparent to 1.0
 
 void main(void) {
 
-    if (isOpaque > 0.5) {
-        gl_FragColor = vec4(eColor.xyz, 1.0);
-    } else {
-        gl_FragColor = eColor;
-    }
+    gl_FragColor = eColor;
 
 }
