dca6c0491299cad1a0e815e3264e4696bd460203
Brian T. Rice <brian@graphistry.com>
2015-09-15 15:13:37 -0700

    JSHint should ignore the PEGJS output.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
 
         jshint: {
-            main: ['src/**/*.js'],
+            main: ['src/**/*.js', '!src/**/expression.js'],
             options: {
                 jshintrc: '.jshintrc',
                 force: true
