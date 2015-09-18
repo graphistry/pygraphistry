b20f41e78c6696c2ffaae1b4ac3395c7560b5dfc
thibaudh <thibaud@graphistry.com>
2015-09-18 14:47:37 -0700

    Version 0.9.10

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ Tutorial and API docs are on
 
 setup(
     name='graphistry',
-    version='0.9.9',
+    version='0.9.10',
     packages = ['graphistry'],
     platforms='any',
     description = 'Visualize node-link graphs using Graphistry\'s cloud',
