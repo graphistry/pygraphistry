9dceefda562552a2d0dcaf0e035c6f06373bae2c
Quinn Johnson <quinn@graphistry.com>
2015-10-20 17:47:14 -0700

    Added fullscreen dark shaders

---
+++ shaders/fullscreendark.fragment.glsl
@@ -0,0 +1,12 @@
+#ifdef GL_ES
+precision highp float;
+#endif
+
+uniform sampler2D uSampler;
+varying vec2 textureCoords;
+
+void main(void) {
+    // TODO: Do this computation once before writing the texture.
+    vec4 color = texture2D(uSampler, textureCoords);
+    gl_FragColor = vec4(color.r, color.g, color.b, 0.33);
+}
