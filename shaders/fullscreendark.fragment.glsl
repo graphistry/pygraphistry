44cd206894977927337186f77c3ae597844c5e3b
Quinn Johnson <quinn@graphistry.com>
2015-10-21 13:59:11 -0700

    fullscreen dark is now darker

--- shaders/fullscreendark.fragment.glsl
+++ shaders/fullscreendark.fragment.glsl
@@ -8,5 +8,5 @@ varying vec2 textureCoords;
 void main(void) {
     // TODO: Do this computation once before writing the texture.
     vec4 color = texture2D(uSampler, textureCoords);
-    gl_FragColor = vec4(color.r, color.g, color.b, 0.33);
+    gl_FragColor = vec4(color.r, color.g, color.b, 0.25);
 }
