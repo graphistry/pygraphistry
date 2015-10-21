25c4291c028b741f0a6d578527defcf184d28ba7
Leo Meyerovich <leo@graphistry.com>
2015-10-21 00:19:33 -0700

    Add config, common, and deploy/ducktape.sh

--- deploy/bundler/bundle.sh
+++ deploy/bundler/bundle.sh
@@ -20,6 +20,13 @@ rm -f $ARCHIVE
 rm -rf $LOGS
 
 ###
+echo "====== BIN ======"
+mkdir -p $OUT/deploy
+cp ../duck-tape-prod.sh $OUT/deploy/duck-tape-prod.sh
+
+
+
+###
 echo "===== INIT LOGS ======"
 mkdir -p $LOGS
 touch $LOGS/git.central.error
@@ -28,6 +35,10 @@ touch $LOGS/git.viz.error
 touch $LOGS/git.viz.log
 touch $LOGS/git.streamgl.error
 touch $LOGS/git.streamgl.log
+touch $LOGS/git.config.error
+touch $LOGS/git.config.log
+touch $LOGS/git.common.error
+touch $LOGS/git.common.log
 touch $LOGS/central.error
 touch $LOGS/central.log
 touch $LOGS/central.helpers.error
@@ -43,10 +54,19 @@ touch $LOGS/viz.log
 
 ### 
 echo "====== DOWNLOAD SOURCES ======"
-mkdir $OUT 
+mkdir -p $OUT 
 git clone https://github.com/graphistry/central.git $OUT/central 2> $LOGS/git.central.error 1> $LOGS/git.central.log
 git clone https://github.com/graphistry/viz-server.git $OUT/viz-server 2> $LOGS/git.viz.error 1> $LOGS/git.viz.log
 git clone https://github.com/graphistry/StreamGl.git $OUT/StreamGL 2> $LOGS/git.streamgl.error 1> $LOGS/git.streamgl.log
+git clone https://github.com/graphistry/config-public.git $OUT/config-public 2> $LOGS/git.config.error 1> $LOGS/git.config.log
+git clone https://github.com/graphistry/common.git $OUT/common 2> $LOGS/git.common.error 1> $LOGS/git.common.log
+
+###
+echo "====== INSTAL CONFIG-PUBLIC ======"
+cd $OUT/config-public
+npm install
+cd ../..
+
 
 ###
 echo "===== INSTALL STREAMGL"
@@ -59,8 +79,10 @@ echo "====== INSTALL CENTRAL ======"
 cd $OUT/central
 npm install 2> ../../$LOGS/central.error 1> ../../$LOGS/central.log
 npm install node_modules/segfault-handler node_modules/node-opencl node_modules/node-pigz 2> ../../$LOGS/central.helpers.error 1> ../../$LOGS/central.helpers.log
-echo "------ COPY STREAMGL ------"
+echo "------ COPY STREAMGL/CONFIG ------"
 cp -r ../StreamGL/ node_modules/StreamGL
+rm -rf node_modules/config
+cp -r ../config-public node_modules/config
 cd ../..
 
 ###
@@ -68,9 +90,16 @@ echo "====== INSTALL VIZ-SERVER ======"
 cd $OUT/viz-server
 npm install 2> ../../$LOGS/viz.error 1> ../../$LOGS/viz.log
 npm install node_modules/segfault-handler node_modules/node-opencl node_modules/node-pigz 2> ../../$LOGS/viz.helpers.error 1> ../../$LOGS/viz.helpers.log
-echo "------ COPY STREAMGL ------"
+echo "------ COPY STREAMGL/CONFIG ------"
 cp -r ../StreamGL/ node_modules/StreamGL
+rm -rf node_modules/config
+cp -r ../config-public node_modules/config
 cd ../..
+
+###
+echo "====== CLEANUP ======"
+rm -rf $OUT/StreamGL
+rm -rf $OUT/config-public
 
 ###
 echo "====== COPY STATIC SOURCES ======"
