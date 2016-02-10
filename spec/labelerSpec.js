c779a06efe4c18b0b9acc1a49f3519c69302c2ef
Quinn Johnson <quinn@graphistry.com>
2016-02-09 16:17:32 -0800

    Added test for labeler without mocking dataframe

--- spec/labelerSpec.js
+++ spec/labelerSpec.js
@@ -1,67 +1,56 @@
 'use strict';
 var _ = require('underscore');
 var labeler = require('../js/labeler.js');
+var Dataframe = require('../js/Dataframe.js');
 
 
 describe('Labeler', function () {
 
     it('should return default labels if none are pre-specified', function () {
+        var dataframe = new Dataframe();
 
-        var graph = {
-            simulator: {},
-            dataframe: {
-                getLabels: _.identity,
-                getDataType: _.identity,
-                getRows: _.identity,
-                doesColumnRepresentColorPaletteMap: _.identity
+        var attrs = {
+            numAttr: {
+                name: 'numAttr', target: 0, type: 'number', values: [5]
+            },
+            strAttr: {
+                name: 'strAttr', target: 0, type: 'string', values: ['bar']
             }
         };
-        graph.simulator.dataframe = graph.dataframe;
 
-        var rows = [
-            {weight: 5, _title: 9, Source: 'Valjean', _index: 9}
-        ];
+        dataframe.load(attrs, 'point', 1);
 
         var expectedResult = [
-          {
-            "title": 9,
-            "columns": [
-              {
-                "value": "Valjean",
-                "key": "Source",
-                "dataType": "string"
-              },
-              {
-                "value": 9,
-                "key": "_index"
-              },
-              {
-                "value": 5,
-                "key": "weight",
-                "dataType": "number"
-              }
-            ]
-          }
+            {
+                "title": 0,
+                "columns": [
+                    {
+                        "value": 0,
+                        "key": "_index"
+                    },
+                    {
+                        "value": 5,
+                        "key": "numAttr",
+                        "dataType": "number"
+                    },
+                    {
+                        "value": "bar",
+                        "key": "strAttr",
+                        "dataType": "string"
+                    }
+                ]
+            }
         ];
 
-        spyOn(graph.dataframe, 'getLabels').andReturn(undefined);
-        spyOn(graph.dataframe, 'getRows').andReturn(rows);
-        spyOn(graph.dataframe, 'doesColumnRepresentColorPaletteMap').andReturn(false);
-        spyOn(graph.dataframe, 'getDataType').andCallFake(function (name) {
-            var type;
-            switch (name) {
-                case 'weight':
-                case '_title':
-                    type = 'number';
-                    break;
-                case 'Source':
-                    type = 'string';
-                    break;
-            }
-            return type;
-        });
+        // Mock graph object
+        var graph = {
+            simulator: {
+                dataframe: dataframe
+            },
+            dataframe: dataframe
+        };
 
-        var labels = labeler.getLabels(graph, [1], 1);
+        var labels = labeler.getLabels(graph, [0], 1);
         expect(labels).toEqual(expectedResult);
     });
 
