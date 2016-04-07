9c28dd85c8d52de2382c4c634e51a1717d9dcef3
thibaudh <thibaud@graphistry.com>
2016-04-07 16:46:39 -0700

    Revert "Upgrade setup.py + readme to reflect that we support python 3.5, not 3.4"
    This reverts commit 8c51d50bd0029d524d8ced746f9c8506c453bb4c. I wrongly
    assumed we tested on Python 3.5. We actually test on Python 3.4

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Scientific/Engineering :: Visualization',
         'Topic :: Scientific/Engineering :: Information Analysis',
     ],
