9e6015891bc82bd37eb12c779a93400319c30db0
lsb <leebutterman@gmail.com>
2016-05-19 16:35:21 -0700

    move httpd restart to top, the js takes time to load

--- deploy/dockerfiles/launch.sh
+++ deploy/dockerfiles/launch.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+docker rm -f graphistry_httpd || true
+
+mkdir -p central-app worker graphistry-json clients reaper
+
+nvidia-docker run --net host --restart=unless-stopped --name graphistry_httpd -d -v `pwd`/central-app:/var/log/central-app -v `pwd`/worker:/var/log/worker -v `pwd`/graphistry-json:/var/log/graphistry-json -v `pwd`/clients:/var/log/clients -v `pwd`/reaper:/var/log/reaper graphistry/central-and-vizservers:$1
+
+
 docker rm -f graphistry_nginx || true
 
 if [ -n "$SSLPATH" ] ; then
@@ -11,13 +18,6 @@ fi
 docker rm -f graphistry_mongo || true
 
 docker run --net host --restart=unless-stopped --name graphistry_mongo -d graphistry/cluster-membership:1.0
-
-
-docker rm -f graphistry_httpd || true
-
-mkdir -p central-app worker graphistry-json clients reaper
-
-nvidia-docker run --net host --restart=unless-stopped --name graphistry_httpd -d -v `pwd`/central-app:/var/log/central-app -v `pwd`/worker:/var/log/worker -v `pwd`/graphistry-json:/var/log/graphistry-json -v `pwd`/clients:/var/log/clients -v `pwd`/reaper:/var/log/reaper graphistry/central-and-vizservers:$1
 
 
 docker rm -f graphistry_splunk || true
