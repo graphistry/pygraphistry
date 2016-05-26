90bd74f5a48511d35f30a8d3189a14a7fd2279b4
lsb <leebutterman@gmail.com>
2016-05-25 17:03:57 -0700

    Pull images before saving

--- deploy/dockerfiles/make-release.sh
+++ deploy/dockerfiles/make-release.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -xe
+docker pull graphistry/nginx-central-vizservers:1.0.0.32 graphistry/nginx-central-vizservers:1.0.0.32.httponly graphistry/cluster-membership:1.0 graphistry/central-and-vizservers:$1
 docker save graphistry/nginx-central-vizservers:1.0.0.32 graphistry/nginx-central-vizservers:1.0.0.32.httponly graphistry/cluster-membership:1.0 graphistry/central-and-vizservers:$1 | gzip -c9 > containers.lxc.gz
 sed -i -e 's_$1_'$1'_' launch.sh
 tar -cvzf iqt-package.tar.gz instructions.txt ../documentation/certs.txt containers.lxc.gz load.sh launch.sh
