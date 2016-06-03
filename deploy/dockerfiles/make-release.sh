077688aa7055f24e61c9c8a127a367f11f52c391
lsb <leebutterman@gmail.com>
2016-06-02 18:53:03 -0700

    Copy certs.txt into place

--- deploy/dockerfiles/make-release.sh
+++ deploy/dockerfiles/make-release.sh
@@ -2,5 +2,6 @@
 for i in graphistry/nginx-central-vizservers:1.0.0.32 graphistry/nginx-central-vizservers:1.0.0.32.httponly graphistry/cluster-membership:1.0 graphistry/central-and-vizservers:$1 ; do docker pull $i ; done
 docker save graphistry/nginx-central-vizservers:1.0.0.32 graphistry/nginx-central-vizservers:1.0.0.32.httponly graphistry/cluster-membership:1.0 graphistry/central-and-vizservers:$1 | gzip -c9 > containers.lxc.gz
 sed -i -e 's_$1_'$1'_' launch.sh
-tar -cvzf iqt-package-${2}.tar.gz instructions.txt ../documentation/certs.txt containers.lxc.gz load.sh launch.sh
+cp ../documentation/certs.txt .
+tar -cvzf iqt-package-${2}.tar.gz instructions.txt certs.txt containers.lxc.gz load.sh launch.sh
 s3cmd -c /home/ubuntu/.s3cfg put iqt-package-${2}.tar.gz s3://graphistry.releases/
