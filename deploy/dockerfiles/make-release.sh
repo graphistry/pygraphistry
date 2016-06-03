20e2dca1e289170bc368a819544ff4db8cbc9b1d
thibaudh <thibaud@graphistry.com>
2016-06-02 22:08:42 -0700

    Make-release: Make tarball name unguessable. Allow sharing via S3 url

--- deploy/dockerfiles/make-release.sh
+++ deploy/dockerfiles/make-release.sh
@@ -2,5 +2,8 @@
 for i in graphistry/nginx-central-vizservers:1.0.0.32 graphistry/nginx-central-vizservers:1.0.0.32.httponly graphistry/cluster-membership:1.0 graphistry/central-and-vizservers:$1 ; do docker pull $i ; done
 docker save graphistry/nginx-central-vizservers:1.0.0.32 graphistry/nginx-central-vizservers:1.0.0.32.httponly graphistry/cluster-membership:1.0 graphistry/central-and-vizservers:$1 | gzip -c9 > containers.lxc.gz
 sed -i -e 's_$1_'$1'_' launch.sh
-tar -cvzf iqt-package-${2}.tar.gz instructions.txt ../documentation/certs.txt containers.lxc.gz load.sh launch.sh
-s3cmd -c /home/ubuntu/.s3cfg put iqt-package-${2}.tar.gz s3://graphistry.releases/
+tar -cvzf tmp.tar.gz instructions.txt ../documentation/certs.txt containers.lxc.gz load.sh launch.sh
+SUFFIX=`sha1sum tmp.tar.gz | cut -d ' ' -f 1`
+TARBALL=iqt-package-${2}-${SUFFIX}.tar.gz
+mv tmp.tar.gz ${TARBALL}
+s3cmd -c /home/ubuntu/.s3cfg put ${TARBALL} s3://graphistry.releases/
