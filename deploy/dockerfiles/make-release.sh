92a951db61a08212bcc4aad673341c8c0308af78
thibaudh <thibaud@graphistry.com>
2016-06-04 11:47:39 -0700

    make-release.sh: lower gzip compression level to gain 5 minutes of packaging at the cost of ~8MB

--- deploy/dockerfiles/make-release.sh
+++ deploy/dockerfiles/make-release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -xe
 for i in graphistry/nginx-central-vizservers:1.0.0.32 graphistry/nginx-central-vizservers:1.0.0.32.httponly graphistry/cluster-membership:1.0 graphistry/central-and-vizservers:$1 ; do docker pull $i ; done
-docker save graphistry/nginx-central-vizservers:1.0.0.32 graphistry/nginx-central-vizservers:1.0.0.32.httponly graphistry/cluster-membership:1.0 graphistry/central-and-vizservers:$1 | gzip -c9 > containers.lxc.gz
+docker save graphistry/nginx-central-vizservers:1.0.0.32 graphistry/nginx-central-vizservers:1.0.0.32.httponly graphistry/cluster-membership:1.0 graphistry/central-and-vizservers:$1 | gzip -c6 > containers.lxc.gz
 sed -i -e 's_$1_'$1'_' launch.sh
 cp ../documentation/certs.txt .
 tar -cvzf tmp.tar.gz instructions.txt certs.txt containers.lxc.gz load.sh launch.sh
