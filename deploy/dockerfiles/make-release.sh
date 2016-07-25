6727534218f56d474aeeb97100216fffe43d96b9
lsb <leebutterman@gmail.com>
2016-07-25 15:47:51 -0700

    Add mongo to the containers that get loaded, to avoid pulling anything.

--- deploy/dockerfiles/make-release.sh
+++ deploy/dockerfiles/make-release.sh
@@ -3,9 +3,10 @@ C1=graphistry/nginx-central-vizservers:1.1.0.32
 C2=graphistry/nginx-central-vizservers:1.1.0.32.httponly
 C3=graphistry/splunkfwd:6.4.1
 C4=graphistry/central-and-vizservers:$1
-for i in    $C1 $C2 $C3 $C4 ; do (docker rmi $i || true) ; docker pull $i ; done
-docker save $C1 $C2 $C3 $C4 | gzip -c6 > containers.lxc.gz
-for i in    $C1 $C2 $C3 $C4 ; do docker rmi $i ; done
+C5=mongo:2
+for i in    $C1 $C2 $C3 $C4 $C5 ; do (docker rmi $i || true) ; docker pull $i ; done
+docker save $C1 $C2 $C3 $C4 $C5 | gzip -c6 > containers.lxc.gz
+for i in    $C1 $C2 $C3 $C4 $C5 ; do docker rmi $i ; done
 sed -i -e 's_$1_'$1'_' launch.sh
 cp ../documentation/certs.txt .
 tar -cvzf tmp.tar.gz instructions.txt certs.txt containers.lxc.gz load.sh launch.sh
