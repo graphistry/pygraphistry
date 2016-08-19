b4f91f1a4537f1bc4608d6a12b7d90f552c3056f
lsb <leebutterman@gmail.com>
2016-08-19 08:28:29 -0700

    Release also to the public; sign a link for each.

--- deploy/dockerfiles/make-release.sh
+++ deploy/dockerfiles/make-release.sh
@@ -4,6 +4,7 @@ C2=graphistry/nginx-central-vizservers:1.1.0.32.httponly
 C3=graphistry/splunkfwd:6.4.1
 C4=graphistry/central-and-vizservers:$1
 C5=mongo:2
+BUCKET=s3://graphistry.releases/
 for i in    $C1 $C2 $C3 $C4 $C5 ; do (docker rmi $i || true) ; docker pull $i ; done
 docker save $C1 $C2 $C3 $C4 $C5 | gzip -c6 > containers.lxc.gz
 for i in    $C1 $C2 $C3 $C4 $C5 ; do docker rmi $i ; done
@@ -13,4 +14,8 @@ tar -cvzf tmp.tar.gz instructions.txt certs.txt containers.lxc.gz load.sh launch
 SUFFIX=`sha1sum tmp.tar.gz | cut -d ' ' -f 1`
 TARBALL=graphistry-release-${2}-${1}-${SUFFIX}.tar.gz
 mv tmp.tar.gz ${TARBALL}
-s3cmd -c /home/ubuntu/.s3cfg put ${TARBALL} s3://graphistry.releases/
+s3cmd -c /home/ubuntu/.s3cfg put ${TARBALL} ${BUCKET}
+
+echo SUCCESS. If you want to share this with your friends, give them the following link, which expires in a month and a half.
+
+s3cmd -c /home/ubuntu/.s3cfg signurl ${BUCKET}${TARBALL} +4000000
