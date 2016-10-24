409bd2fcba5b2bcb6451eb5d45019aebee58053f
lsb <leebutterman@gmail.com>
2016-10-24 13:57:12 -0700

    Follow redirects.

--- deploy/smoketests/001-etl.sh
+++ deploy/smoketests/001-etl.sh
@@ -4,10 +4,10 @@
 
 WHOAMI=twoetls
 SEEKRIT=Validated
-APIKEY=$(curl --silent $1'/api/encrypt?text='${WHOAMI}${SEEKRIT} | awk -F '"' '{print $(NF-1)}')
+APIKEY=$(curl -L --silent $1'/api/encrypt?text='${WHOAMI}${SEEKRIT} | awk -F '"' '{print $(NF-1)}')
 DATASETNAME=g
 
-for i in {1..2} ; do echo -n ${1}'/graph/graph.html?dataset='${DATASETNAME}'&viztoken=' ; curl --silent -X POST -H "Content-Type: application/json" --data '{"name":"'${DATASETNAME}'","graph":[{"s":"a","d":"b"},{"s":"b","d":"c"}],"bindings":{"sourceField":"s","destinationField":"d"}}' ${1}'/etl?key='${APIKEY} | awk -F '"' '{print $(NF-1)}' ; done
+for i in {1..2} ; do echo -n ${1}'/graph/graph.html?dataset='${DATASETNAME}'&viztoken=' ; curl -L --silent -X POST -H "Content-Type: application/json" --data '{"name":"'${DATASETNAME}'","graph":[{"s":"a","d":"b"},{"s":"b","d":"c"}],"bindings":{"sourceField":"s","destinationField":"d"}}' ${1}'/etl?key='${APIKEY} | awk -F '"' '{print $(NF-1)}' ; done
 
 echo Check out the above link to see the result of a successful ETL.
 
