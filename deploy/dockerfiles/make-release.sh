494e60a7df68f8f32991143cc4a8c5169fd1e410
lsb <leebutterman@gmail.com>
2016-10-24 23:40:01 -0700

    Less compression in the tar file

--- deploy/dockerfiles/make-release.sh
+++ deploy/dockerfiles/make-release.sh
@@ -11,11 +11,11 @@ docker save $C1 $C2 $C3 $C4 $C5 $C6 | pigz -b500 > containers.lxc.gz
 for i in    $C1 $C2 $C3 $C4 $C5 $C6 ; do docker rmi $i ; done
 sed -i -e 's_$1_'$1'_' launch.sh
 cp ../documentation/certs.txt .
-tar -cvzf tmp.tar.gz instructions.txt certs.txt containers.lxc.gz load.sh launch.sh
+GZIP=-1 tar -cvzf tmp.tar.gz instructions.txt certs.txt containers.lxc.gz load.sh launch.sh
 SUFFIX=`sha1sum tmp.tar.gz | cut -d ' ' -f 1`
 TARBALL=graphistry-release-${2}-${1}-${SUFFIX}.tar.gz
 mv tmp.tar.gz ${TARBALL}
-s3cmd -c /home/ubuntu/.s3cfg put ${TARBALL} ${BUCKET}
+s3cmd -c /home/ubuntu/.s3cfg --multipart-chunk-size-mb=100 put ${TARBALL} ${BUCKET}
 
 echo SUCCESS. If you want to share this with your friends, give them the following link, which expires in a month and a half.
 
