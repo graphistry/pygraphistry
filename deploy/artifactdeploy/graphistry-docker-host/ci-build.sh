aff521637b9eb34aaed8ad7368879daa4e4d79a6
lsb <leebutterman@gmail.com>
2017-01-12 15:27:43 -0800

    Staging and labs config, injected

--- deploy/artifactdeploy/graphistry-docker-host/ci-build.sh
+++ deploy/artifactdeploy/graphistry-docker-host/ci-build.sh
@@ -4,11 +4,17 @@ KEY_PATH=${SECRETS}/aws/ansible_id_rsa.pem
 TLS_PATH=${SECRETS}/tls
 SPLUNK_UP=${SECRETS}/internalsplunk/userpassword
 SPLUNK_AP=${SECRETS}/internalsplunk/adminpassword
+SPLUNK_PP=${SECRETS}/internalsplunk/pivotpassword
 echo "---
 splunk_user_password: $(cat $SPLUNK_UP)
 splunk_admin_password: $(cat $SPLUNK_AP)
-splunk_deploy_server_uri: "splunk.graphistry.com:8089"
+splunk_deploy_server_uri: splunk.graphistry.com:8089
+pivot_app_splunk_user: admin
+pivot_app_splunk_key: ${SPLUNK_PP}
+pivot_app_splunk_host: splunk.graphistry.com
 inventory_friendlyname: ${HOSTNAMEOVERRIDE:-HOST}
+vizapp_s3_access: dev1087
+vizapp_s3_secret: dev1087
 ansible_ssh_user: ${BOXUSER}
 ansible_ssh_private_key_file: \"$(pwd)/${KEY_PATH}\"" > deploy/artifactdeploy/graphistry-docker-host/group_vars/all
 
@@ -17,6 +23,6 @@ cp -r ${TLS_PATH} deploy/artifactdeploy/graphistry-docker-host/files/certs
 chmod 400 ${KEY_PATH}
 ssh -T -i ${KEY_PATH} ${BOXUSER}@${HOST} whoami
 cd deploy/artifactdeploy/graphistry-docker-host
-echo $HOST > inventory
+echo "[$(echo $HOSTNNAMEOVERRIDE | cut -d - -f 1)]\n$HOST" > inventory
 ansible-playbook setup.yml -i inventory
 
