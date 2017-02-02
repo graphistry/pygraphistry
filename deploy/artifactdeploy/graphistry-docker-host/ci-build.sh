174d564cc40b3b31cbe2d1bd4126e8556bff88ab
lsb <leebutterman@gmail.com>
2017-02-01 18:30:12 -0800

    Use real s3 credentials for viz-app

--- deploy/artifactdeploy/graphistry-docker-host/ci-build.sh
+++ deploy/artifactdeploy/graphistry-docker-host/ci-build.sh
@@ -4,6 +4,8 @@ KEY_PATH=${SECRETS}/aws/ansible_id_rsa.pem
 SPLUNK_UP=${SECRETS}/internalsplunk/userpassword
 SPLUNK_AP=${SECRETS}/internalsplunk/adminpassword
 SPLUNK_PP=${SECRETS}/internalsplunk/pivotpassword
+VIZAPP_ACCESS=${SECRETS}/aws/s3/vizapp/access
+VIZAPP_SECRET=${SECRETS}/aws/s3/vizapp/secret
 echo "---
 splunk_user_password: $(cat $SPLUNK_UP)
 splunk_admin_password: $(cat $SPLUNK_AP)
@@ -12,8 +14,8 @@ pivot_app_splunk_user: admin
 pivot_app_splunk_key: $(cat $SPLUNK_PP)
 pivot_app_splunk_host: splunk.graphistry.com
 inventory_friendlyname: ${HOSTNAMEOVERRIDE:-HOST}
-vizapp_s3_access: dev1087
-vizapp_s3_secret: dev1087
+vizapp_s3_access: $(cat VIZAPP_ACCESS)
+vizapp_s3_secret: $(cat VIZAPP_SECRET)
 ansible_ssh_user: ${BOXUSER}
 ansible_ssh_private_key_file: \"$(pwd)/${KEY_PATH}\"" > deploy/artifactdeploy/graphistry-docker-host/group_vars/all
 
