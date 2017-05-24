5d0c78bbf2e62510cbb18385ff8d5260a3337ab8
lsb <leebutterman@gmail.com>
2017-05-24 14:11:42 -0700

    Allow specifying the amount of CPU parallelism

--- deploy/dockerfiles/launch.sh
+++ deploy/dockerfiles/launch.sh
@@ -92,7 +92,7 @@ echo "${VIZ_APP_CONFIG:-{\}}" > ./viz-app-config.json
 [[ -d ../viz-app-config.json ]] && rm -rf ../viz-app-config.json
 stat ../viz-app-config.json || (echo '{}' > ../viz-app-config.json)
 
-NPROC=$(((which nproc > /dev/null) && nproc) || echo 1)
+NPROC=${NPROC:-$(((which nproc > /dev/null) && nproc) || echo 1)}
 CENTRAL_MERGED_CONFIG=$(docker   run --rm -v ${PWD}/../httpd-config.json:/tmp/box-config.json -v ${PWD}/httpd-config.json:/tmp/local-config.json graphistry/central-and-vizservers:$1${HTTPD_IMAGE_SUFFIX} bash -c 'mergeThreeFiles.js $graphistry_install_path/central-cloud-options.json    /tmp/box-config.json /tmp/local-config.json')
 VIZWORKER_MERGED_CONFIG=$(docker run --rm -e NPROC=$NPROC -v ${PWD}/../httpd-config.json:/tmp/box-config.json -v ${PWD}/httpd-config.json:/tmp/local-config.json graphistry/central-and-vizservers:$1${HTTPD_IMAGE_SUFFIX} bash -c '(envsubst < $graphistry_install_path/viz-worker-cloud-options.json.envsubst > /tmp/default-config.json) && mergeThreeFiles.js /tmp/default-config.json /tmp/box-config.json /tmp/local-config.json')
 
