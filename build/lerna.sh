87c0e88803d054c5c7ba843ecb6a5ce75d40f130
Paul Taylor <paul.e.taylor@me.com>
2017-10-24 15:42:47 -0700

    attempt to fix first-day build failure by always calling docker run (#265)

--- build/lerna.sh
+++ build/lerna.sh
@@ -35,34 +35,34 @@ for ARG in "$@"; do
     esac
 done
 
-# graphistry dockerhub account/container namespace ("graphistry")
-G_NS="$GRAPHISTRY_NAMESPACE"
-# The lerna container name
-LERNA_CONTAINER="$G_NS/lerna"
-
 docker build \
     -f build/Dockerfile-lerna \
-    --build-arg NAMESPACE=${G_NS} \
-    -t ${LERNA_CONTAINER} .
+    --build-arg NAMESPACE=${GRAPHISTRY_NAMESPACE} \
+    -t lerna .
 
 if [[ ! $SHOULD_RUN_LERNA_CMD && ! $SHOULD_RUN_SCRIPT ]]; then
     exit 0
 fi
 
-trap "rv=\$?; docker rm -f lerna || true; exit \$rv" EXIT
-
-docker rm -f lerna || true
-docker run -i -d --name lerna -v "${PWD}":/${G_NS} ${LERNA_CONTAINER} sh
-
 if [ $SHOULD_RUN_LERNA_CMD ]; then
-    docker exec -t lerna sh -c "$LERNA_CMD_TO_RUN"
+    docker run --rm -v "${PWD}":/${GRAPHISTRY_NAMESPACE} lerna ${LERNA_CMD_TO_RUN}
 fi
 
 if [ $SHOULD_RUN_SCRIPT ]; then
+
+    LS_NAMES_CMD="$LERNA_LS_COMMAND -- echo \$LERNA_PACKAGE_NAME"
+    LS_PATHS_CMD="$LERNA_LS_COMMAND -- echo \${PWD##*/$GRAPHISTRY_NAMESPACE/}"
+
     # get module package paths e.g. packages/viz-app, packages/legacy/config, etc.
-    PACKAGE_PATHS=($(docker exec -t lerna $LERNA_LS_COMMAND -- echo \${PWD##*/$G_NS/} | tr -d '\r'))
+    PACKAGE_PATHS=($(docker run --rm \
+        -v "${PWD}":/${GRAPHISTRY_NAMESPACE} \
+        lerna $LS_PATHS_CMD | tr -d '\r'))
+
     # get module package names e.g. @graphistry/viz-app, @graphistry/config, etc.
-    PACKAGE_NAMES=($(docker exec -t lerna $LERNA_LS_COMMAND -- echo \$LERNA_PACKAGE_NAME | tr -d '\r'))
+    PACKAGE_NAMES=($(docker run --rm \
+        -v "${PWD}":/${GRAPHISTRY_NAMESPACE} \
+        lerna $LS_NAMES_CMD | tr -d '\r'))
+
     for ((i=0; $i<${#PACKAGE_PATHS[*]}; i++)); do
         PACKAGE_PATH="${PACKAGE_PATHS[i]}"
         PACKAGE_NAME="${PACKAGE_NAMES[i]}"
@@ -74,7 +74,7 @@ if [ $SHOULD_RUN_SCRIPT ]; then
         ROOT_PATH="$PWD" \
         PACKAGE_PATH="$PACKAGE_PATH" \
         PACKAGE_NAME="$PACKAGE_NAME" \
-        CONTAINER_NAME="$G_NS/${PACKAGE_NAME##*@*/}" \
+        CONTAINER_NAME="$GRAPHISTRY_NAMESPACE/${PACKAGE_NAME##*@*/}" \
             ./"$PACKAGE_PATH/build/$SCRIPT_TO_RUN"
     done
 fi
