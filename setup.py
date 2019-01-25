d6b49cb62ead199a2bf0e0e2db771776e1c26b85
Christopher Harris <xixonia@gmail.com>
2019-01-24 21:31:29 -0600

    .

--- setup.py
+++ setup.py
@@ -3,35 +3,36 @@
 from setuptools import setup, find_packages
 import versioneer
 
-long_description = """
-**PyGraphistry** is a visual graph analytics library to extract, transform, and
-load big graphs into `Graphistry's <http://www.graphistry.com>`_ GPU-cloud-accelerated
-explorer.
+long_description =
+    """
+    **PyGraphistry** is a visual graph analytics library to extract, transform, and
+    load big graphs into `Graphistry's <http://www.graphistry.com>`_ GPU-cloud-accelerated
+    explorer.
 
-PyGraphistry is...
+    PyGraphistry is...
 
-- **Fast & Gorgeous**: Cluster, filter, and inspect large amounts of data at
-  interactive speed. We layout graphs with a descendant of the gorgeous
-  ForceAtlas2 layout algorithm introduced in Gephi. Our data explorer connects
-  to Graphistry's GPU cluster to layout and render hundreds of thousand of
-  nodes+edges in your browser at unparalleled speeds.
+    - **Fast & Gorgeous**: Cluster, filter, and inspect large amounts of data at
+    interactive speed. We layout graphs with a descendant of the gorgeous
+    ForceAtlas2 layout algorithm introduced in Gephi. Our data explorer connects
+    to Graphistry's GPU cluster to layout and render hundreds of thousand of
+    nodes+edges in your browser at unparalleled speeds.
 
-- **Notebook Friendly**: PyGraphistry plays well with interactive notebooks
-  like IPython/Juypter, Zeppelin, and Databricks: Process, visualize, and drill
-  into with graphs directly within your notebooks.
+    - **Notebook Friendly**: PyGraphistry plays well with interactive notebooks
+    like IPython/Juypter, Zeppelin, and Databricks: Process, visualize, and drill
+    into with graphs directly within your notebooks.
 
-- **Batteries Included**: PyGraphistry works out-of-the-box with popular data
-  science and graph analytics libraries. It is also very easy to use. To create
-  the visualization shown above, download this dataset of Facebook communities
-  from SNAP and load it with your favorite library
+    - **Batteries Included**: PyGraphistry works out-of-the-box with popular data
+    science and graph analytics libraries. It is also very easy to use. To create
+    the visualization shown above, download this dataset of Facebook communities
+    from SNAP and load it with your favorite library
 
 
-Try It Out!
------------
+    Try It Out!
+    -----------
 
-Tutorial and API docs are on
-`https://github.com/graphistry/pygraphistry <https://github.com/graphistry/pygraphistry>`_
-"""
+    Tutorial and API docs are on
+    `https://github.com/graphistry/pygraphistry <https://github.com/graphistry/pygraphistry>`_
+    """
 
 setup(
     name='graphistry',
@@ -84,7 +85,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',        
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering :: Visualization',
         'Topic :: Scientific/Engineering :: Information Analysis',
     ],
