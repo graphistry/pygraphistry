ee5406bbf4990faa0fa495fb9fe268320a032969
Leo Meyerovich <leo@graphistry.com>
2019-07-26 21:44:01 -0400

    fix(tiger import): wip

--- graphistry/__init__.py
+++ graphistry/__init__.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
--- graphistry/plugins/tigergraph.py
+++ graphistry/plugins/tigergraph.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 import requests
 import pandas as pd
 
