b255efe3ea6172b3fc051e314fd16a2c34a25c8e
Leo Meyerovich <leo@graphistry.com>
2021-02-09 18:06:38 -0800

    garden(sphix): refresh base autogen

--- docs/build.sh
+++ docs/build.sh
@@ -3,10 +3,13 @@
 EXCLUDES="../setup.py ../graphistry/util.py"
 echo "SKIPPING " $EXCLUDES
 sphinx-apidoc -o source .. $EXCLUDES
-make html
+
+make clean
+make html SPHINXOPTS="-W --keep-going -n"
 
 PLATFORM=`uname`
 if [[ "$PLATFORM" == "Darwin" ]]
 then
+    echo "Opening in Darwin.."
     open build/html/index.html
 fi
--- docs/docker.sh
+++ docs/docker.sh
@@ -2,11 +2,14 @@
 
 mkdir -p build
 
+RUN_INSTALLS="(cp -r /doc /pygraphistry && cd /pygraphistry && python -m pip install -e .[dev] )"
+RUN_SPHINX="cd docs && ./build.sh || ( echo 'Printing /tmp/sphinx*' && cat /tmp/sphinx* && exit 1 )"
+
+#TODO make a docker layer so we can cache RUN_INSTALLS
 docker run \
     --entrypoint=/bin/bash \
     --rm -it \
     -e USER_ID=$UID \
     -v $(pwd)/..:/doc \
     ddidier/sphinx-doc:3.2.1-1 \
-    -c "pip install pandas numpy pyarrow && cd docs && ./build.sh"
-
+    -c "${RUN_INSTALLS} && ${RUN_SPHINX}"
