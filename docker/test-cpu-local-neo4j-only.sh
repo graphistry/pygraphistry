3a5cbf6fcfcfd968e1b584d3fce8b3d097242b21
Leo Meyerovich <leo@graphistry.com>
2021-04-29 17:09:53 -0700

    fix(neo4j test): abs pyenv path and speedup test

--- docker/test-cpu-local-neo4j-only.sh
+++ docker/test-cpu-local-neo4j-only.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -ex
 
+PYTHON_VERSION=${PYTHON_VERSION:-3.6}
 WITH_NEO4J=1
 WITH_SUDO=${WITH_SUDO:-sudo}
 
 ( cd ../test/db/neo4j && WITH_SUDO="$WITH_SUDO" ./launch.sh )
 
-docker-compose build
+docker-compose build --build-arg PYTHON_VERSION=${PYTHON_VERSION} test-cpu
 
 TEST_CPU_VERSION=${TEST_CPU_VERSION:-latest}
 
 ${WITH_SUDO} docker run \
+    --security-opt seccomp=unconfined \
     -e PYTEST_CURRENT_TEST=TRUE \
     -e WITH_NEO4J=1 \
     --rm \
