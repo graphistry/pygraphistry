c88a5761258c75dae250280770790f6dcddd0021
Alex <alex@stayopen.com>
2022-01-12 15:00:06 -0800

    fixes typo from `graphistry` to `pygraphistry`

--- ml/__init__.py
+++ ml/__init__.py
@@ -5,16 +5,16 @@ class DefaulMLConfig():
     
 # Get sample data
 def get_host_dataframe(nrows=100000):
-    df = pd.read_csv('graphistry/data/host_events.csv', nrows=nrows, index_col=0)
+    df = pd.read_csv('pygraphistry/data/host_events.csv', nrows=nrows, index_col=0)
     return df
 
 
 def get_netflow_dataframe(nrows=100000):
-    df = pd.read_csv('graphistry/data/netflow_events.csv', nrows=nrows, index_col=0)
+    df = pd.read_csv('pygraphistry/data/netflow_events.csv', nrows=nrows, index_col=0)
     return df
 
 def get_botnet_dataframe(nrows=100000):
-    df = pd.read_csv('graphistry/data/malware_capture_bot.csv', nrows=nrows, index_col=0)
+    df = pd.read_csv('pygraphistry/data/malware_capture_bot.csv', nrows=nrows, index_col=0)
     # cast things as strings or floats
     as_string = ['StartTime', 'Proto', 'SrcAddr', 'Sport', 'DstAddr', 'Dir',
      'Dport', 'State']
