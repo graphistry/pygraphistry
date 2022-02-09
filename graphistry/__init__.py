ff2fe7ed13781d69412f1a03b20c440dda734a6d
silkspaceships <silkspace@users.noreply.github.com>
2022-02-09 13:55:02 -0800

    Update __init__.py

--- graphistry/__init__.py
+++ graphistry/__init__.py
@@ -1,4 +1,4 @@
-from graphistry.ai import *
+from graphistry import ai
 from graphistry.pygraphistry import (  # noqa: E402, F401
     client_protocol_hostname, protocol, server,
     register, privacy, login, refresh, api_token, verify_token,
