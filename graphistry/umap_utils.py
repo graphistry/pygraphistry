99b04c55642d64394fb9dbc2e448822a286e09de
Alex <alex@stayopen.com>
2022-02-09 18:57:03 -0800

    adds timing on umap

--- graphistry/umap_utils.py
+++ graphistry/umap_utils.py
@@ -1,4 +1,5 @@
 # a base class for UMAP to run on cpu or gpu
+from time import time
 from typing import Union
 
 import numpy as np
@@ -12,7 +13,7 @@ logger = setup_logger(__name__)
 
 umap_kwargs_probs = {
     "n_components": 2,
-    "metric": "hellinger",  # info metric
+    "metric": "hellinger",  # info metric, can't use on textual encodings since they contain negative values...
     "n_neighbors": 15,
     "min_dist": 0.3,
 }
@@ -42,15 +43,20 @@ class BaseUMAPMixin(umap.UMAP):
             return y
         else:
             logger.warning(
-                f"Ignoring target column of shape {y.shape} as it is not one dimensional"
+                f"* Ignoring target column of shape {y.shape} as it is not one dimensional"
             )
             return None
 
     def fit(self, X: np.ndarray, y: Union[np.ndarray, None] = None):
+        t = time()
         y = self._check_target_is_one_dimensional(y)
+        logger.info(f'Starting UMAP-ing data of shape {X.shape}')
         super().fit(X, y)
         self._is_fit = True
         self._edge_influence()
+        mins = (time()-t)/60
+        logger.info(f'-UMAP-ing took {mins:.2f} minutes total')
+        logger.info(f' - or {X.shape[0]/mins:.2f} rows per minute')
         return self
 
     def fit_transform(self, X: np.ndarray, y: Union[np.ndarray, None] = None):
