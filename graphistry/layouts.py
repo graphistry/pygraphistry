1ce512c15aad5c15c636640b4a383c30b6997e2a
Swa <swa@orbifold.net>
2022-03-10 11:04:21 +0100

    Some refactoring.

--- graphistry/layouts.py
+++ graphistry/layouts.py
@@ -26,7 +26,7 @@ class LayoutsMixin(MIXIN_BASE):
                     width: Optional[float] = None,
                     height: Optional[float] = None,
                     allow_cycles = True,
-                    roots=None,
+                    root=None,
                     *args,
                     **kwargs):
         """
@@ -62,7 +62,7 @@ class LayoutsMixin(MIXIN_BASE):
             if SugiyamaLayout.has_cycles(g._edges, source_column = g2._source, target_column = g2._destination):
                 raise ValueError
 
-        triples = SugiyamaLayout.arrange(g2._edges, topological_coordinates = True, source_column = g2._source, target_column = g2._destination, include_levels = True, roots = roots)
+        triples = SugiyamaLayout.arrange(g2._edges, topological_coordinates = True, source_column = g2._source, target_column = g2._destination, include_levels = True, root = root)
         g2._nodes[level_col] = [triples[id][2] for id in g2._nodes[g2._node]]
         g2._nodes[y_col] = [triples[id][1] * height for id in g2._nodes[g2._node]]
         if (g2._nodes is None) or (len(g2._nodes) == 0):
