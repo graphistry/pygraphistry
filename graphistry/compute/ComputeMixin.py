9a9749ec7015bf35d8d126fb08c25fc450707087
lmeyerov <leo@graphistry.com>
2022-04-07 00:23:29 -0700

    Dev/query (#327)
    * refactor(compute): move compute into a folder
    
    * fix(api=1 categoricals): serializer warning
    
    * infra(harden): treat warnings as errors
    
    * docs(ComputeMixin): handle move
    
    * refactor(hop): move to standalone file
    
    * infra(lint): allow better method defs
    
    * feat(filter_by_dict): including node, edge variants
    
    * fix(lint): new line warning
    
    * feat(hop edge predicates): with extern file refactor
    
    * fix(compute): add type defs
    
    * feat(hops): add pre and post pattern filtering
    
    * refactor(node_match): pre/post to soruce/destination
    
    * refactor(node_match): pre/post to soruce/destination

--- graphistry/compute/ComputeMixin.py
+++ graphistry/compute/ComputeMixin.py
@@ -3,7 +3,12 @@ import logging
 
 import pandas as pd
 
-from .Plottable import Plottable
+from graphistry.Plottable import Plottable
+from .hop import hop as hop_base
+from .filter_by_dict import (
+    filter_edges_by_dict as filter_edges_by_dict_base,
+    filter_nodes_by_dict as filter_nodes_by_dict_base
+)
 
 logger = logging.getLogger('compute')
 
@@ -214,93 +219,14 @@ class ComputeMixin(MIXIN_BASE):
             out_df = g2_base._nodes.merge(nodes_df[[g2_base._node, level_col]], on = g2_base._node, how = 'left')
             return self.nodes(out_df)
 
-    def hop(self, nodes, hops: Optional[int] = 1, to_fixed_point: bool = False, direction: str = 'forward'):
-        """
-        Given a graph and some source nodes, return subgraph of all paths within k-hops from the sources
+    def hop(self, *args, **kwargs):
+        return hop_base(self, *args, **kwargs)
+    hop.__doc__ = hop_base.__doc__
 
-        g: Plotter
-        nodes: dataframe with id column matching g._node
-        hops: how many hops to consider, if any bound
-        to_fixed_point: keep hopping until no new nodes are found
-        direction: 'forward', 'backwards', 'undirected'
+    def filter_nodes_by_dict(self, *args, **kwargs):
+        return filter_nodes_by_dict_base(self, *args, **kwargs)
+    filter_nodes_by_dict.__doc__ = filter_nodes_by_dict_base.__doc__
 
-        - currently only supports forwards hops
-        - does not yet support transitive closure and backwards/undirected hops
-        """
-
-        if not to_fixed_point and not isinstance(hops, int):
-            raise ValueError(f'Must provide hops int when to_fixed_point is False, received: {hops}')
-
-        g2 = self.materialize_nodes()
-
-        edges_indexed = g2._edges.reset_index()
-        EDGE_ID = 'index'
-
-        hops_remaining = hops
-        wave_front = nodes[[ g2._node ]]
-        matches_nodes = wave_front
-        matches_edges = edges_indexed[[EDGE_ID]][:0]
-
-        while True:
-            if not to_fixed_point and hops_remaining is not None:
-                if hops_remaining < 1:
-                    break
-                hops_remaining = hops_remaining - 1
-
-            hop_edges_forward = None
-            new_node_ids_forward = None
-            if direction in ['forward', 'undirected']:
-                hop_edges_forward = (
-                    wave_front.merge(
-                        edges_indexed[[g2._source, g2._destination, EDGE_ID]].rename(columns={g2._source: g2._node}),
-                        how='inner',
-                        on=g2._node)
-                    [[g2._destination, EDGE_ID]]
-                )
-                new_node_ids_forward = hop_edges_forward[[g2._destination]].rename(columns={g2._destination: g2._node}).drop_duplicates()
-
-            hop_edges_reverse = None
-            new_node_ids_reverse = None
-            if direction in ['reverse', 'undirected']:
-                hop_edges_reverse = (
-                    wave_front.merge(
-                        edges_indexed[[g2._destination, g2._source, EDGE_ID]].rename(columns={g2._destination: g2._node}),
-                        how='inner',
-                        on=g2._node)
-                    [[g2._source, EDGE_ID]]
-                )
-                new_node_ids_reverse = hop_edges_reverse[[g2._source]].rename(columns={g2._source: g2._node}).drop_duplicates()
-
-            new_node_ids = pd.concat(
-                []
-                    + ( [ new_node_ids_forward ] if new_node_ids_forward is not None else [] )  # noqa: W503
-                    + ( [ new_node_ids_reverse] if new_node_ids_reverse is not None else [] ),  # noqa: W503
-                ignore_index=True, sort=False).drop_duplicates()
-            combined_node_ids = pd.concat([matches_nodes, new_node_ids], ignore_index=True, sort=False).drop_duplicates()
-
-            matches_edges = pd.concat(
-                [ matches_edges]
-                + ([ hop_edges_forward[[ EDGE_ID ]] ] if hop_edges_forward is not None else [])  # noqa: W503
-                + ([ hop_edges_reverse[[ EDGE_ID ]] ] if hop_edges_reverse is not None else []),  # noqa: W503
-                ignore_index=True, sort=False).drop_duplicates(subset=[EDGE_ID])
-
-            if len(combined_node_ids) == len(matches_nodes):
-                #fixedpoint, exit early: future will come to same spot!
-                break
-        
-            wave_front = new_node_ids
-            matches_nodes = combined_node_ids
-
-
-        #hydrate edges
-        final_edges = edges_indexed.merge(matches_edges, on=EDGE_ID, how='inner')
-        if EDGE_ID not in self._edges:
-            final_edges = final_edges.drop(columns=[EDGE_ID])
-        g_out = g2.edges(final_edges)
-
-        #hydrate nodes
-        if self._nodes is not None:
-            final_nodes = self._nodes.merge(matches_nodes, on=self._node, how='inner')
-            g_out = g_out.nodes(final_nodes)
-
-        return g_out
+    def filter_edges_by_dict(self, *args, **kwargs):
+        return filter_edges_by_dict_base(self, *args, **kwargs)
+    filter_edges_by_dict.__doc__ = filter_edges_by_dict_base.__doc__
---
+++ graphistry/compute/hop.py
@@ -0,0 +1,153 @@
+from typing import List, Optional
+import pandas as pd
+
+from graphistry.Plottable import Plottable
+from .filter_by_dict import filter_by_dict
+
+
+def hop(self: Plottable,
+    nodes: Optional[pd.DataFrame] = None,
+    hops: Optional[int] = 1,
+    to_fixed_point: bool = False,
+    direction: str = 'forward',
+    edge_match: Optional[dict] = None,
+    source_node_match: Optional[dict] = None,
+    destination_node_match: Optional[dict] = None
+) -> Plottable:
+    """
+    Given a graph and some source nodes, return subgraph of all paths within k-hops from the sources
+
+    g: Plotter
+    nodes: dataframe with id column matching g._node. None signifies all nodes (default).
+    hops: how many hops to consider, if any bound (default 1)
+    to_fixed_point: keep hopping until no new nodes are found (ignores hops)
+    direction: 'forward', 'reverse', 'undirected'
+    edge_match: dict of kv-pairs to exact match (see also: filter_edges_by_dict)
+    source_node_match: dict of kv-pairs to match nodes before hopping
+    destination_node_match: dict of kv-pairs to match nodes after hopping (including intermediate)
+    """
+
+    if not to_fixed_point and not isinstance(hops, int):
+        raise ValueError(f'Must provide hops int when to_fixed_point is False, received: {hops}')
+
+    if direction not in ['forward', 'reverse', 'undirected']:
+        raise ValueError(f'Invalid direction: "{direction}", must be one of: "forward" (default), "reverse", "undirected"')
+
+    if destination_node_match == {}:
+        destination_node_match = None
+
+    g2 = self.materialize_nodes()
+
+    if nodes is None:
+        nodes = g2._nodes
+
+    edges_indexed = g2.filter_edges_by_dict(edge_match)._edges.reset_index()
+    EDGE_ID = 'index'
+
+    hops_remaining = hops
+    wave_front = filter_by_dict(nodes[[ g2._node ]], source_node_match)
+    matches_nodes = None
+    matches_edges = edges_indexed[[EDGE_ID]][:0]
+
+    while True:
+        if not to_fixed_point and hops_remaining is not None:
+            if hops_remaining < 1:
+                break
+            hops_remaining = hops_remaining - 1
+
+        hop_edges_forward = None
+        new_node_ids_forward = None
+        if direction in ['forward', 'undirected']:
+            hop_edges_forward = (
+                wave_front.merge(
+                    edges_indexed[[g2._source, g2._destination, EDGE_ID]].assign(**{g2._node: edges_indexed[g2._source]}),
+                    how='inner',
+                    on=g2._node)
+                [[g2._source, g2._destination, EDGE_ID]]
+            )
+            new_node_ids_forward = hop_edges_forward[[g2._destination]].rename(columns={g2._destination: g2._node}).drop_duplicates()
+
+            if destination_node_match is not None:
+                new_node_ids_forward = filter_by_dict(
+                    g2._nodes.merge(new_node_ids_forward, on=g2._node, how='inner'),
+                    destination_node_match
+                )[[g2._node]]
+                hop_edges_forward = hop_edges_forward.merge(
+                    new_node_ids_forward.rename(columns={g2._node: g2._destination}),
+                    how='inner',
+                    on=g2._destination
+                )
+
+        hop_edges_reverse = None
+        new_node_ids_reverse = None
+        if direction in ['reverse', 'undirected']:
+            hop_edges_reverse = (
+                wave_front.merge(
+                    edges_indexed[[g2._destination, g2._source, EDGE_ID]].assign(**{g2._node: edges_indexed[g2._destination]}),
+                    how='inner',
+                    on=g2._node)
+                [[g2._destination, g2._source, EDGE_ID]]
+            )
+            new_node_ids_reverse = hop_edges_reverse[[g2._source]].rename(columns={g2._source: g2._node}).drop_duplicates()
+
+            if destination_node_match is not None:
+                new_node_ids_reverse = filter_by_dict(
+                    g2._nodes.merge(new_node_ids_reverse, on=g2._node, how='inner'),
+                    destination_node_match
+                )[[g2._node]]
+                hop_edges_reverse = hop_edges_reverse.merge(
+                    new_node_ids_reverse.rename(columns={g2._node: g2._source}),
+                    how='inner',
+                    on=g2._source
+                )
+
+        mt : List[pd.DataFrame] = []  # help mypy
+
+        matches_edges = pd.concat(
+            [ matches_edges ]
+            + ([ hop_edges_forward[[ EDGE_ID ]] ] if hop_edges_forward is not None else mt)  # noqa: W503
+            + ([ hop_edges_reverse[[ EDGE_ID ]] ] if hop_edges_reverse is not None else mt),  # noqa: W503
+            ignore_index=True, sort=False).drop_duplicates(subset=[EDGE_ID])
+
+        new_node_ids = pd.concat(
+            mt
+                + ( [ new_node_ids_forward ] if new_node_ids_forward is not None else mt )  # noqa: W503
+                + ( [ new_node_ids_reverse] if new_node_ids_reverse is not None else mt ),  # noqa: W503
+            ignore_index=True, sort=False).drop_duplicates()
+
+        # Finally add initial nodes as confirmed also match edge + post-node predicates, not just pre-node predicates 
+        if matches_nodes is None:
+            matches_nodes = pd.concat(
+                mt
+                    + ( [hop_edges_forward[[g2._source]].rename(columns={g2._source: g2._node}).drop_duplicates()]  # noqa: W503
+                        if hop_edges_forward is not None
+                        else mt)
+                    + ( [hop_edges_reverse[[g2._destination]].rename(columns={g2._destination: g2._node}).drop_duplicates()]  # noqa: W503
+                        if hop_edges_reverse is not None
+                        else mt),
+                ignore_index=True, sort=False).drop_duplicates(subset=[g2._node])
+
+        combined_node_ids = pd.concat([matches_nodes, new_node_ids], ignore_index=True, sort=False).drop_duplicates()
+
+        if len(combined_node_ids) == len(matches_nodes):
+            #fixedpoint, exit early: future will come to same spot!
+            break
+    
+        wave_front = new_node_ids
+        matches_nodes = combined_node_ids
+
+    #hydrate edges
+    final_edges = edges_indexed.merge(matches_edges, on=EDGE_ID, how='inner')
+    if EDGE_ID not in self._edges:
+        final_edges = final_edges.drop(columns=[EDGE_ID])
+    g_out = g2.edges(final_edges)
+
+    #hydrate nodes
+    if self._nodes is not None:
+        final_nodes = self._nodes.merge(
+            matches_nodes if matches_nodes is not None else wave_front[:0],
+            on=self._node,
+            how='inner')
+        g_out = g_out.nodes(final_nodes)
+
+    return g_out
