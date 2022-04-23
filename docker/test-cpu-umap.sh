f644544cdedb6fdba7f06fd11941d0a06067fa0d
Leo Meyerovich <leo@graphistry.com>
2022-04-23 12:13:30 -0400

    fix(docker): default to kominos model

--- docker/test-cpu-umap.sh
+++ docker/test-cpu-umap.sh
@@ -2,6 +2,7 @@
 set -ex
 
 PYTHON_VERSION=${PYTHON_VERSION:-3.6}
+SENTENCE_TRANSFORMER=${SENTENCE_TRANSFORMER-average_word_embeddings_komninos}
 WITH_SUDO=${WITH_SUDO:-}
 
 COMPOSE_DOCKER_CLI_BUILD=1 \
@@ -9,6 +10,7 @@ DOCKER_BUILDKIT=1 \
 docker-compose build \
     --build-arg PYTHON_VERSION=${PYTHON_VERSION} \
     --build-arg PIP_DEPS="-e .[umap-learn,test]" \
+    --build-arg SENTENCE_TRANSFORMER="${SENTENCE_TRANSFOMER}" \
     test-cpu
 
 TEST_CPU_VERSION=${TEST_CPU_VERSION:-latest}
