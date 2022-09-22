71d3e9c047f2157b78deb0c9f766f2dc4ff68962
Vaim Dev <dev@v-aim.com>
2022-09-22 17:13:43 +0800

    feat (test scripts): add test script for the register function

--- graphistry/tests/test_pygraphistry.py
+++ graphistry/tests/test_pygraphistry.py
@@ -2,7 +2,7 @@
 
 import unittest, pytest
 
-from graphistry import PyGraphistry
+from graphistry.pygraphistry import PyGraphistry
 
 # TODO mock requests for testing actual effectful code
 
@@ -21,4 +21,24 @@ class TestPyGraphistry_Auth(unittest.TestCase):
 def test_register_with_only_username(capfd):
     PyGraphistry.register(username='only_username')
     out, err = capfd.readouterr()
-    assert out == "Hello World!"
+    assert "username exists but missing password" in out
+
+def test_register_with_only_password(capfd):
+    PyGraphistry.register(password='only_password')
+    out, err = capfd.readouterr()
+    assert "password exist but missing username" in out
+
+def test_register_with_only_personal_key_id(capfd):
+    PyGraphistry.register(personal_key='only_personal_key_id')
+    out, err = capfd.readouterr()
+    assert "Error: personal key exists but missing personal key id" in out
+
+def test_register_with_only_personal_key(capfd):
+    PyGraphistry.register(personal_key='only_personal_key')
+    out, err = capfd.readouterr()
+    assert "Error: personal key id exists but missing personal key" in out
+
+def test_register_with_only_personal_key(capfd):
+    PyGraphistry.register(personal_key='only_personal_key')
+    out, err = capfd.readouterr()
+    assert "Error: personal key id exists but missing personal key" in out
