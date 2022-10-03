7bf4b814420aa00362b84e256637ad61a41a714d
tanmoyio <tanmoyf2@gmail.com>
2022-10-03 19:00:29 +0530

    RotatE added

--- graphistry/embed_utils.py
+++ graphistry/embed_utils.py
@@ -1,13 +1,16 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from tqdm import trange
 
 class HeterographEmbedModuleMixin(nn.Module):
     def __init__(self):
         super().__init__()
 
         self.protocol = {
-                'TransE': self.TransE
+                'TransE': self.TransE,
+                'DistMult': self.DistMult,
+                'RotatE': self.RotatE
         }
 
     def embed(self, proto='TransE', d=128, gamma=12):
@@ -40,18 +43,20 @@ class HeterographEmbedModuleMixin(nn.Module):
 
         for s, r, d in zip(src, relation, dst):
             triplets.append([s, r, d])
-
         # initialize the dataloader
         dataset = EmbedDataset(entity2id, relation2id, triplets)
         train_generator = DataLoader(dataset, batch_size=32) # need autoscale batch for gpu? [AUTOML]
 
         # training loop
-        # optim (TODO: ADAM)
-        optim = torch.optim.SGD(em_mod.parameters(), lr=0.01)
+        optim = torch.optim.Adam(em_mod.parameters(), lr=0.01)
 
         # TODO: define epoch [AUTOML]
-        for _ in range(10):
 
+        epoch = trange(1000, leave=True)
+        for _ in epoch:
+            loss_acc, c = 0, 0
+
+            #for _ in trange(1000):
             em_mod.train()
             for h, r, t in train_generator:
 
@@ -68,21 +73,30 @@ class HeterographEmbedModuleMixin(nn.Module):
 
                 loss = em_mod(pos_triples, neg_triples)
                 loss.mean().backward()
-                print(loss.mean().item()) 
+                loss_acc += loss.mean().item()
+                c += 1
                 optim.step()
 
-        # out vectors
-        # --------------------------------------
-        # exception handling will be added later
+            epoch.set_description(f"loss: {loss_acc/c}")
+            epoch.refresh()
+
+        self.node_em = em_mod.node_em.weight.detach().numpy()
+        self.edge_em = em_mod.edge_em.weight.detach().numpy()
+        return self.node_em, self.edge_em
 
 
     def TransE(self, h, r, t):
-        gamma = 12
-        o = h + r - t
-        return gamma - (o/o.max()) # will be replaced by 2 norm later
+        return (h + r - t).norm(p=1, dim=1)
 
     def TransD(self, h, r, t):
         return
+
+    def DistMult(self, h, r, t):
+        return (h * r * t).sum(dim=-1)
+
+    def RotatE(self, h, r, t):
+        return -(h * r - t).norm(p=1, dim=1)
+        
 
 
 class HeteroEmbed(nn.Module):
@@ -97,11 +111,11 @@ class HeteroEmbed(nn.Module):
         self.uniform_(self.node_em)
         self.uniform_(self.edge_em)
 
-        self.criterion = nn.MarginRankingLoss()
+        self.criterion = nn.MarginRankingLoss(margin=1, reduction='none')
         self.proto = proto
 
     def uniform_(self, x):
-        x.weight.data.uniform_(-self.erange, -self.erange)
+        x.weight.data.uniform_(-self.erange, self.erange)
      
     def forward(self, pos_triplets, neg_triplets):
 
@@ -116,7 +130,8 @@ class HeteroEmbed(nn.Module):
         return self.loss(pos_dist, neg_dist)
 
     def loss(self, pos_dist, neg_dist):
-        target = torch.ones_like(pos_dist, dtype=torch.long) * -1
+        target = torch.tensor([-1], dtype=torch.long)
+        # target = torch.ones_like(pos_dist, dtype=torch.long) * -1
         return self.criterion(pos_dist, neg_dist, target)
     
     def trainer(self):
