e31cd54ce4eb2d89b02558adf506e5fc49479591
Alex <alex@stayopen.com>
2022-10-20 15:27:29 -0700

    commented out

--- graphistry/outliers.py
+++ graphistry/outliers.py
@@ -10,9 +10,9 @@ from sklearn.covariance import EllipticEnvelope
 from sklearn.svm import OneClassSVM
 
 import random
-# our library
-import shap
-import dr_explainer as dre # pip install cluster-shapley
+
+# import shap
+# import dr_explainer as dre # pip install cluster-shapley
 
 
 # #####################################################################################################################
@@ -38,9 +38,6 @@ def get_embedding_extent(embedding):
     print(f'min extent x: {mminx}, max extent x: {mmaxx}, delta: {deltax}')
     print(f'min extent y: {mminy}, max extent y: {mmaxy}, delta: {deltay}')
     return mminx, mmaxx, mminy, mmaxy
-
-
-
 
 
 def plot_outliers(embedding, classifiers, name, xy_extent=((0, 10), (0, 10)), figsize=(7, 4), xy=(1,1), xytext=(2,2), border=1):
@@ -151,117 +148,117 @@ def detect_outliers(embedding, name='data', contamination=0.25, gamma=0.35, xy_e
 #     shap_values = clusterShapley.transform(X)
 #     return shap_values
 
-def fit_clust(X, y):
-    clust = shap.utils.hclust(X, y, linkage="single")
-    return clust
+# def fit_clust(X, y):
+#     clust = shap.utils.hclust(X, y, linkage="single")
+#     return clust
 
-def fit_shapely(X, model):
-    explainer = shap.Explainer(model)
-    shap_values = explainer(X)
-    return shap_values, explainer
+# def fit_shapely(X, model):
+#     explainer = shap.Explainer(model)
+#     shap_values = explainer(X)
+#     return shap_values, explainer
 
-def fit_explanation(X, shap_values):
-    print(f'Explaining features: {X.columns}')
+# def fit_explanation(X, shap_values):
+#     print(f'Explaining features: {X.columns}')
     
-    c_exp = shap.Explanation(shap_values, data=X, feature_names=X.columns)
-    return c_exp
+#     c_exp = shap.Explanation(shap_values, data=X, feature_names=X.columns)
+#     return c_exp
 
-def fit_explainer(X, model):
-    # takes outside model and fits it on data
-    explainer = shap.Explainer(model)
-    shap_values = explainer(X)
-    return explainer, shap_values
+# def fit_explainer(X, model):
+#     # takes outside model and fits it on data
+#     explainer = shap.Explainer(model)
+#     shap_values = explainer(X)
+#     return explainer, shap_values
     
-def _handle_index(shap_values, klass):
-    if klass is None:
-        values = shap_values[klass]
-    else:
-        values = shap_values
-    return values
+# def _handle_index(shap_values, klass):
+#     if klass is None:
+#         values = shap_values[klass]
+#     else:
+#         values = shap_values
+#     return values
 
-def beeswarm(shap_values, klass):
-    # takes explanation fit model and plots as beeswarm
-    shap_values = _handle_index(shap_values, klass)
-    shap.plots.beeswarm(shap_values)
+# def beeswarm(shap_values, klass):
+#     # takes explanation fit model and plots as beeswarm
+#     shap_values = _handle_index(shap_values, klass)
+#     shap.plots.beeswarm(shap_values)
 
-def waterfall(shap_values, klass):
-    shap_values = _handle_index(shap_values, klass)
-    # visualize the first prediction's explanation
-    shap.plots.waterfall(shap_values)
+# def waterfall(shap_values, klass):
+#     shap_values = _handle_index(shap_values, klass)
+#     # visualize the first prediction's explanation
+#     shap.plots.waterfall(shap_values)
     
-def force(shap_values, klass):
-    shap_values = _handle_index(shap_values, klass)
-    shap.plots.force(shap_values)
+# def force(shap_values, klass):
+#     shap_values = _handle_index(shap_values, klass)
+#     shap.plots.force(shap_values)
 
-def bar(shap_values, clust=None):
-    shap.plots.bar(shap_values, clustering=clust, clustering_cutoff=1)
-
-
-
-class HFMixin():
-    def __init__(self):
-        from transformers import pipeline
-        self.pipeline = pipeline
+# def bar(shap_values, clust=None):
+#     shap.plots.bar(shap_values, clustering=clust, clustering_cutoff=1)
 
 
-class SentimentMixin(HFMixin):
-    def __init__(self):
-        self.model = self.pipeline('sentiment-analysis', return_all_scores=True)
+
+# class HFMixin():
+#     def __init__(self):
+#         from transformers import pipeline
+#         self.pipeline = pipeline
+
+
+# class SentimentMixin(HFMixin):
+#     def __init__(self):
+#         self.model = self.pipeline('sentiment-analysis', return_all_scores=True)
         
         
-class ShapelyMixin():
+# class ShapelyMixin():
     
-    def __init__(self, X: pd.DataFrame, y:pd.DataFrame, name: str):
-        self.X = X
-        self.y = y
-        self.name = name
-        self.feature_names = X.columns
-        self._model_is_fit = False
+#     def __init__(self, X: pd.DataFrame, y:pd.DataFrame, name: str):
+#         self.X = X
+#         self.y = y
+#         self.name = name
+#         self.feature_names = X.columns
+#         self._model_is_fit = False
 
     
-    def fit(self, model: Any = None):
-        self.fit_shapely()
-        self.fit_explanation(self.X)
-        if model is not None:
-            self.shap_external_model_scores = self.fit_model_explainer(self.X, model)
+#     def fit(self, model: Any = None):
+#         self.fit_shapely()
+#         self.fit_explanation(self.X)
+#         if model is not None:
+#             self.shap_external_model_scores = self.fit_model_explainer(self.X, model)
             
-    # def fit_shapely(self):
-    #     print('Fitting a Supervised Shapely Cluster model')
-    #     self.cluster_shapely = fit_shapely(self.X, self.y)
-    #
-    # def transform(self, X):
-    #     shap_values = explain_shapely(X, self.cluster_shapely)
-    #     return shap_values
+#     # def fit_shapely(self):
+#     #     print('Fitting a Supervised Shapely Cluster model')
+#     #     self.cluster_shapely = fit_shapely(self.X, self.y)
+#     #
+#     # def transform(self, X):
+#     #     shap_values = explain_shapely(X, self.cluster_shapely)
+#     #     return shap_values
 
-    # def fit_explanation(self, X):
-    #     print('Fitting a Semi-Supervised Shapely Explanation model '
-    #           '-- it explains the data from the perspective of the shapely_scores of the Shapely Cluster Model')
-    #     #shap_values = self.transform(X)
-    #     self.explanation_model = fit_explanation(X, shap_values, self.feature_names)
+#     # def fit_explanation(self, X):
+#     #     print('Fitting a Semi-Supervised Shapely Explanation model '
+#     #           '-- it explains the data from the perspective of the shapely_scores of the Shapely Cluster Model')
+#     #     #shap_values = self.transform(X)
+#     #     self.explanation_model = fit_explanation(X, shap_values, self.feature_names)
         
-    def fit_model_explainer(self, X: pd.DataFrame, model: Any):
-        print('Fitting a Explainer model '
-              '-- it explains the data from the input Model')
+#     def fit_model_explainer(self, X: pd.DataFrame, model: Any):
+#         print('Fitting a Explainer model '
+#               '-- it explains the data from the input Model')
 
-        self.explainer_model, shap_values = fit_explainer(X, model)
-        self._model_is_fit = True
-        return shap_values
+#         self.explainer_model, shap_values = fit_explainer(X, model)
+#         self._model_is_fit = True
+#         return shap_values
     
-    def explain(self, X: pd.DataFrame):
-        if self._model_is_fit:
-            shap_values = self.explainer_model(X)
-            return shap_values
-        else:
-            raise NotImplementedError('No model has been fit, fit instance using `g.explain.fit`')
+#     def explain(self, X: pd.DataFrame):
+#         if self._model_is_fit:
+#             shap_values = self.explainer_model(X)
+#             return shap_values
+#         else:
+#             raise NotImplementedError('No model has been fit, fit instance using `g.explain.fit`')
 
-    def plot(self, X: pd.DataFrame, kind='bees', klass=None):
-        shap_values = self.explain(X)
-        if kind == 'bees':
-            beeswarm(shap_values, klass)
-        if kind == 'waterfall':
-            waterfall(shap_values, klass)
-        if kind == 'force':
-            force(shap_values, klass)
+#     def plot(self, X: pd.DataFrame, kind='bees', klass=None):
+#         shap_values = self.explain(X)
+#         if kind == 'bees':
+#             beeswarm(shap_values, klass)
+#         if kind == 'waterfall':
+#             waterfall(shap_values, klass)
+#         if kind == 'force':
+#             force(shap_values, klass)
         
     
 
