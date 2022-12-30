bb261fa710bd9246df846e564ab3b847d6deeac1
Alex <alex@stayopen.com>
2022-12-29 22:33:26 -0800

    feat(support for dbscan over feature cols or umap embeddings)

--- graphistry/compute/cluster.py
+++ graphistry/compute/cluster.py
@@ -14,11 +14,32 @@ if TYPE_CHECKING:
     MIXIN_BASE = Plottable
 else:
     MIXIN_BASE = object
+
+
+def lazy_dbscan_import_has_dependency():
+    has_min_dependency = True
+    DBSCAN = None
+    try:
+        from sklearn.cluster import DBSCAN
+    except ImportError:
+        has_min_dependency = False
+        logger.warning('Please install sklearn for CPU DBSCAN')
+
+    has_cuml_dependency = True
+    cuDBSCAN = None
+    try:
+        from cuml import DBSCAN as cuDBSCAN
+    except ImportError:
+        has_cuml_dependency = False
+        logger.warning('Please install cuml for GPU DBSCAN')
+    
+    return has_min_dependency, DBSCAN, has_cuml_dependency, cuDBSCAN
+
     
     
-def cluster(g, dbscan, kind='nodes'):
+def get_umap_embedding_df(g, kind='nodes'):
     """
-        Fits clustering on UMAP embeddings
+        Returns a dataframe with the UMAP embeddings from the graphistry graph
     """
     if kind == 'nodes':
         df = g._node_embedding
@@ -27,6 +48,30 @@ def cluster(g, dbscan, kind='nodes'):
     else:
         raise ValueError('kind must be one of nodes or edges')
         
+    return df
+    
+def cluster(g, dbscan, kind='nodes', cols=None, umap=True):
+    """
+        Fits clustering on UMAP embeddings if umap is True, otherwise on the features dataframe
+        
+        args:
+            g: graphistry graph
+            kind: 'nodes' or 'edges'
+            cols: list of columns to use for clustering given `g.featurize` has been run
+            umap: whether to use UMAP embeddings or features dataframe
+    """
+    
+    if cols is None:
+        df = g._get_feature(kind)
+    else: 
+        df = g.get_features_by_cols(cols, kind)
+
+    if umap and cols is None: 
+        df = get_umap_embedding_df(g, kind)
+    
+    print(df.head())
+
+    
     dbscan.fit(df)
     labels = dbscan.labels_
     
@@ -35,7 +80,7 @@ def cluster(g, dbscan, kind='nodes'):
     elif kind == 'edges':
         g._edges['_cluster'] = labels
     else:
-        raise ValueError('kind must be one of nodes or edges')
+        raise ValueError('kind must be one of `nodes` or `edges`')
     
     kind = 'node' if kind == 'nodes' else 'edge'
     setattr(g, f'_{kind}_dbscan', dbscan)
@@ -46,34 +91,28 @@ class ClusterMixin(MIXIN_BASE):
     def __init__(self, *args, **kwargs):
         pass
     
-    def _cluster_dbscan(self, res, kind, eps, min_samples, **kwargs):
+    def _cluster_dbscan(self, res, kind, cols, umap, eps, min_samples, **kwargs):
         """
             DBSCAN clustering on cpu or gpu infered by umap's .engine flag
         """
-        if self.engine == UMAP_LEARN:
-            try:
-                from sklearn.cluster import DBSCAN
-            except ImportError:
-                raise ImportError('Please install sklearn')
-            
-        elif self.engine == CUML:
-            try:
-                from cuml import DBSCAN as cuDBSCAN
-            except ImportError:
-                raise ImportError('Please install cuml')
-        else:
-            raise ValueError(f'engine must be one of {UMAP_LEARN} or {CUML}')
+        _, DBSCAN, _, cuDBSCAN = lazy_dbscan_import_has_dependency()
         
         dbscan = cuDBSCAN(eps=eps, min_samples=min_samples, **kwargs) if self.engine == CUML else DBSCAN(eps=eps, min_samples=min_samples, **kwargs)
-        res = cluster(res, dbscan, kind=kind)
+        res = cluster(res, dbscan, kind=kind, cols=cols, umap=umap)
 
         return res
     
-    def dbscan(self, kind='nodes', eps: float = 1., min_samples: int = 1, **kwargs):
-        """DBSCAN clustering
+    
+    def dbscan(self, kind = 'nodes', cols = None, umap = True, eps: float = 1., min_samples: int = 1, **kwargs):
+        """DBSCAN clustering on cpu or gpu infered by umap's .engine flag
+        
+        Args:
+            kind: 'nodes' or 'edges'
+            cols: list of columns to use for clustering given `g.featurize` has been run, nice way to slice features by fragments of interest, e.g. ['ip172', 'location', 'asn', 'warnings']
+            umap: whether to use UMAP embeddings or features dataframe
         """
         res = self.bind()
-        res = self._cluster_dbscan(res, kind=kind, eps=eps, min_samples=min_samples, **kwargs)
+        res = res._cluster_dbscan(res, kind=kind, cols=cols, umap=umap, eps=eps, min_samples=min_samples, **kwargs)
         
         return res
 
