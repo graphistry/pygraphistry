8b94be9496bffccd4d39ea083715742ca2ee774a
Alex <alex@stayopen.com>
2023-01-03 00:54:46 -0800

    feat(large refactor of transform methods from featurize, umap, and dbscan, adding `infer_graph` nearest graph interpolation in umap or featurize coordinates)

--- graphistry/compute/cluster.py
+++ graphistry/compute/cluster.py
@@ -1,5 +1,7 @@
 import logging
 import pandas as pd
+import numpy as np
+
 from typing import Any, List, Union, TYPE_CHECKING
 from typing_extensions import Literal
 from collections import Counter
@@ -7,6 +9,9 @@ from collections import Counter
 from graphistry.Engine import Engine
 from graphistry.Plottable import Plottable
 from graphistry.constants import CUML, UMAP_LEARN  # noqa type: ignore
+from graphistry.features import ModelDict
+from graphistry.feature_utils import get_matrix_by_column_parts
+from graphistry.ai_utils import infer_graph
 
 logger = logging.getLogger("compute.cluster")
 
@@ -59,7 +64,22 @@ def resolve_cpu_gpu_engine(
     )
 
     
-def cluster(g, dbscan, kind='nodes', cols=None, umap=True):
+def get_model_matrix(g, kind, cols, umap):
+    assert kind in ['nodes', 'edges']
+    assert hasattr(g, '_node_encoder') if kind == 'nodes' else hasattr(g, '_edge_encoder')
+    
+    if cols is None:
+        df = g._get_feature(kind)
+    else: 
+        df = g.get_features_by_cols(cols, kind)
+
+    if umap and cols is None and g._umap is not None: 
+        df = g._get_embedding(kind)
+        
+    return df
+    
+
+def dbscan_fit(g, dbscan, kind='nodes', cols=None, umap=True):
     """
         Fits clustering on UMAP embeddings if umap is True, otherwise on the features dataframe
         
@@ -69,22 +89,15 @@ def cluster(g, dbscan, kind='nodes', cols=None, umap=True):
             cols: list of columns to use for clustering given `g.featurize` has been run
             umap: whether to use UMAP embeddings or features dataframe
     """
-    
-    if cols is None:
-        df = g._get_feature(kind)
-    else: 
-        df = g.get_features_by_cols(cols, kind)
+    df = get_model_matrix(g, kind, cols, umap)
 
-    if umap and cols is None and g._umap is not None: 
-        df = g._get_embedding(kind)
-    
     dbscan.fit(df)
     labels = dbscan.labels_
     
     if kind == 'nodes':    
-        g._nodes['_cluster'] = labels
+        g._nodes = g._nodes.assign(_dbscan = labels)
     elif kind == 'edges':
-        g._edges['_cluster'] = labels
+        g._nodes = g._edges.assign(_dbscan = labels)
     else:
         raise ValueError('kind must be one of `nodes` or `edges`')
     
@@ -93,20 +106,53 @@ def cluster(g, dbscan, kind='nodes', cols=None, umap=True):
     
     return g
 
+
+
+def dbscan_predict(X: pd.DataFrame, model):
+    """
+        DBSCAN has no predict per se, so we reverse engineer one here
+        from https://stackoverflow.com/questions/27822752/scikit-learn-predicting-new-points-with-dbscan
+    """
+    n_samples = X.shape[0]
+    
+    y_new = np.ones(shape=n_samples, dtype=int) * -1
+        
+    for i in range(n_samples):
+        diff = model.components_ - X.iloc[i, :].values  # NumPy broadcasting
+
+        dist = np.linalg.norm(diff, axis=1)  # Euclidean distance
+
+        shortest_dist_idx = np.argmin(dist)
+
+        if dist[shortest_dist_idx] < model.eps:
+            y_new[i] = model.labels_[model.core_sample_indices_[shortest_dist_idx]]
+
+    return y_new
+
+def dbscan_predict2(g, kind='nodes', cols=None, umap=True):
+    X = g._get_feature(kind)
+    dbscan = g._node_dbscan if kind == 'nodes' else g._edge_dbscan
+
+    preds = dbscan_predict(X, dbscan)
+    return X, preds
+                                      
+
 class ClusterMixin(MIXIN_BASE):
     def __init__(self, *args, **kwargs):
         pass
     
     def _cluster_dbscan(self, res, kind, cols, umap, eps, min_samples, **kwargs):
         """
-            DBSCAN clustering on cpu or gpu infered by umap's .engine flag
+            DBSCAN clustering on cpu or gpu infered by .engine flag
         """
         _, DBSCAN, _, cuDBSCAN = lazy_dbscan_import_has_dependency()
         
         res.engine = resolve_cpu_gpu_engine("auto")
+        res._kwargs_dbscan = ModelDict('latest dbscan kwargs', kind=kind, cols=cols, umap=umap, eps=eps, min_samples=min_samples, **kwargs)
         
         dbscan = cuDBSCAN(eps=eps, min_samples=min_samples, **kwargs) if res.engine == CUML else DBSCAN(eps=eps, min_samples=min_samples, **kwargs)
-        res = cluster(res, dbscan, kind=kind, cols=cols, umap=umap)
+        
+        res = dbscan_fit(res, dbscan, kind=kind, cols=cols, umap=umap)
 
         return res
     
@@ -140,21 +186,76 @@ class ClusterMixin(MIXIN_BASE):
             g2.plot() # color by `_cluster`
 
         Useful:
-            Enriching the graph with cluster labels from UMAP is useful for visualizing clusters in the graph by color, size, etc.
-            see https://github.com/graphistry/pygraphistry/blob/master/demos/ai/cyber/cyber-redteam-umap-demo.ipynb
+            Enriching the graph with cluster labels from UMAP is useful for visualizing clusters in the graph by color, size, etc, 
+            as well as assessing metrics per cluster, e.g.
+             https://github.com/graphistry/pygraphistry/blob/master/demos/ai/cyber/cyber-redteam-umap-demo.ipynb
                         
         Args:
             kind: 'nodes' or 'edges'
-            cols: list of columns to use for clustering given `g.featurize` has been run, nice way to slice features by fragments of interest, e.g. ['ip_172', 'location', 'ssh', 'warnings']
+            cols: list of columns to use for clustering given `g.featurize` has been run, nice way to slice features by 
+                fragments of interest, e.g. ['ip_172', 'location', 'ssh', 'warnings']
             umap: whether to use UMAP embeddings or features dataframe
             eps: The maximum distance between two samples for them to be considered as in the same neighborhood.
-            min_samples: The number of samples in a neighborhood for a point to be considered as a core point. This includes the point itself.
+            min_samples: The number of samples in a neighborhood for a point to be considered as a core point. 
+                This includes the point itself.
             
         """
         res = self.bind()
         res = res._cluster_dbscan(res, kind=kind, cols=cols, umap=umap, eps=eps, min_samples=min_samples, **kwargs)
         
         return res
-
-    # def _is_cudf(self, df):
-    #     return 'cudf' in str(type(df))
+    
+    def _transform_dbscan(self, df: pd.DataFrame, ydf=None, kind: str='nodes') -> pd.DataFrame:
+        """Transforms a dataframe to one with a new column '_cluster' containing the DBSCAN cluster labels
+            and returns feature[cols] or UMAP embedding
+        Examples:
+            fit: 
+                g = graphistry.edges(edf, 'src', 'dst').nodes(ndf, 'node')
+                g2 = g.featurize().dbscan()
+                
+            predict:
+                labels = g2.transform_dbscan(ndf)
+        
+        """
+        
+        res = self.bind()
+        if hasattr(res, '_kwargs_dbscan'):
+            # Assume that we are transforming to last fit of dbscan
+            cols = res._kwargs_dbscan['cols']
+            umap = res._kwargs_dbscan['umap']
+            
+            dbscan = res._node_dbscan if kind == 'nodes' else res._edge_dbscan
+            
+            emb = None
+            if umap:
+                emb, X, y = res.transform_umap(df, ydf, kind=kind, return_graph=False)
+            else:
+                  X, _ = res.transform(df, ydf, kind=kind)
+            if cols is not None:
+                X = get_matrix_by_column_parts(X, cols)
+    
+            if umap:
+                X_ = emb
+            else:
+                X_ = X
+                
+            labels = dbscan_predict(X_, dbscan)
+            df = df.assign(_dbscan=labels, x=emb.x, y=emb.y)
+            return emb, X, y, df
+        else:
+            raise Exception('No dbscan model found. Please run `g.dbscan()` first')
+        
+    def transform_dbscan(self, df, y=None, eps=30, use_umap_embedding=True, n_nearest=None, kind='nodes', return_graph=True):
+        """Transforms a dataframe to one with a new column '_cluster' containing the DBSCAN cluster labels on the minibatch
+            if return_graph is True, then a graph is returned with the minibatch added to the existing graph
+            if return_graph is False, then the enriched minibatch dataframe, features, and UMAP embedding are returned
+            
+        """
+        emb, X, y, df = self._transform_dbscan(df, y, kind=kind)
+        if return_graph:
+            res = self.bind()
+            g = infer_graph(res, emb, X, y, df, use_umap=use_umap_embedding, eps=eps, sample=n_nearest) 
+            return g
+        return emb, X, df
+    
+    
