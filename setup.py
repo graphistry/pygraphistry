6315a7ff50241edefd51ef2d487cff84d38efe5a
Tanmoy <tanmoyf2@gmail.com>
2023-03-01 01:32:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ base_extras_light = {
     'networkx': ['networkx>=2.5'],
     'gremlin': ['gremlinpython'],
     'bolt': ['neo4j', 'neotime'],
-    'nodexl': ['openpyxl', 'xlrd'],
+    'nodexl': ['openpyxl==3.1.0', 'xlrd'],
     'jupyter': ['ipython'],
 }
 
