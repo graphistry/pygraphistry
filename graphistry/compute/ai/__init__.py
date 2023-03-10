f0c73779bf4008436c34fbf11e22399964e4c587
Alex <alex@stayopen.com>
2023-03-10 13:55:02 -0800

    temp removes langchain and gpt

--- graphistry/compute/ai/__init__.py
+++ graphistry/compute/ai/__init__.py
@@ -1,6 +1,6 @@
 
 from . import ai_prompts
-from . import gpt_index_utils
-from . import langchain_utils
+#from . import gpt_index_utils
+#from . import langchain_utils
 from . import symbolic
 from . import utils
