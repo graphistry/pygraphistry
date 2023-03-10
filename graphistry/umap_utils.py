812f5c7a76b65be29d39fb7f2abe5b0d3812ca0c
Desirree Adegunle <87389186+dess890@users.noreply.github.com>
2023-03-10 12:50:23 -0500

    fix(docstr): removed unneccessary spacing

--- graphistry/umap_utils.py
+++ graphistry/umap_utils.py
@@ -426,14 +426,14 @@ class UMAPMixin(MIXIN_BASE):
         """UMAP the featurized nodes or edges data, or pass in your own X, y (optional) dataframes of values
         
         Example
-        -------
+     
         >>> import graphistry   
         >>> g = graphistry.nodes(pd.DataFrame({'node': [0,1,2], 'data': [1,2,3], 'meta': ['a', 'b', 'c']}))
         >>> g2 = g.umap(n_components=3, spread=1.0, min_dist=0.1, n_neighbors=12, negative_sample_rate=5, local_connectivity=1, repulsion_strength=1.0, metric='euclidean', suffix='', play=0, encode_position=True, encode_weight=True, dbscan=False, engine='auto', feature_engine='auto', inplace=False, memoize=True, verbose=False)
         >>> g2.plot()
         
         Parameters
-        ----------
+ 
             :X: either a dataframe ndarray of features, or column names to featurize
             :y: either an dataframe ndarray of targets, or column names to featurize
                     targets
