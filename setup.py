8b1331154a494c3ac93695e43d47abfc88417b8a
dcolinmorgan <apple@dc-m1.lan>
2023-03-31 10:30:09 +0800

    placeholder cu_cat setup

--- setup.py
+++ setup.py
@@ -41,9 +41,10 @@ base_extras_light = {
 base_extras_heavy = {
   'umap-learn': ['umap-learn', 'dirty-cat==0.2.0', 'scikit-learn>=1.0'],
 }
+# base_extras_heavy['cu_cat'] = ['cu-cat @ git+https://github.com/graphistry/cu_cat.git@cudf-cat']
 
 # https://github.com/facebookresearch/faiss/issues/1589 for faiss-cpu 1.6.1, #'setuptools==67.4.0' removed
-base_extras_heavy['ai'] = base_extras_heavy['umap-learn'] + ['scipy', 'dgl', 'torch', 'sentence-transformers', 'faiss-cpu', 'joblib']
+base_extras_heavy['ai'] = base_extras_heavy['umap-learn'] + ['scipy', 'dgl', 'torch', 'sentence-transformers', 'faiss-cpu', 'joblib'] #+ base_extras_heavy['cu_cat']
 
 base_extras = {**base_extras_light, **base_extras_heavy}
 
