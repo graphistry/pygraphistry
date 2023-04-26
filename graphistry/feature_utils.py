d38f469e23b833943d5d64e8cf76e3bcc9775529
dcolinmorgan <dcolinmorgan@gmail.com>
2023-04-26 10:29:53 +0800

    use dirty_cat superVec for torch/etc, except if cu_cat

--- graphistry/feature_utils.py
+++ graphistry/feature_utils.py
@@ -960,8 +960,8 @@ def process_dirty_dataframes(
     :return: Encoded data matrix and target (if not None),
             the data encoder, and the label encoder.
     """
-    # if feature_engine == 'dirty_cat':
-    from dirty_cat import SuperVectorizer, GapEncoder, SimilarityEncoder
+    if feature_engine != 'cu_cat':
+        from dirty_cat import SuperVectorizer, GapEncoder, SimilarityEncoder
     if feature_engine == 'cu_cat':
         lazy_import_has_cu_cat_dependancy()  # tried to use this rather than importing below
         from cu_cat import SuperVectorizer, GapEncoder, SimilarityEncoder
