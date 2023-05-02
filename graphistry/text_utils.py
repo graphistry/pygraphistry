162ee824f0395f6ea7f37afa550a68672890ad55
Leo Meyerovich <leo@graphistry.com>
2023-05-01 19:55:44 -0700

    fix(lint)

--- graphistry/text_utils.py
+++ graphistry/text_utils.py
@@ -246,7 +246,7 @@ class SearchToGraphMixin(MIXIN_BASE):
             feats = res._node_features.iloc[found_indices]  # type: ignore
             if res._umap is not None:
                 emb = res._node_embedding.iloc[found_indices]  # type: ignore
-        except Exception as e:  # for explicit relabeled nodes
+        except Exception:  # for explicit relabeled nodes
             #logger.exception(e)
             tdf = rdf[df[node].isin(found_indices)]
             feats = res._node_features.loc[tdf.index]  # type: ignore
