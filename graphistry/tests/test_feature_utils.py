a22e85eb466b3a252910d91a2377a9c21bdf0f2b
Daniel <dcolinmorgan@gmail.com>
2023-08-30 16:42:14 +0800

    type checking node attributes causing issues

--- graphistry/tests/test_feature_utils.py
+++ graphistry/tests/test_feature_utils.py
@@ -356,8 +356,7 @@ class TestFeatureMethods(unittest.TestCase):
             ndf = g._edges
             self.cases_check_edge_attributes(g)
 
-        cols = ndf.columns
-        
+        # cols = ndf.columns
         # assert np.all(ndf == df[cols]), f"Graphistry {kind}-dataframe does not match outside dataframe it was fed"
 
     def _test_featurizations(self, g, use_cols, targets, name, kind, df):
