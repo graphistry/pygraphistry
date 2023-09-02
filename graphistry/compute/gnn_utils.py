5c69c19683f7412058ee11532fbb3b492ea09f2e
Alex Morrise <desitter.gravity@gmail.com>
2023-09-02 11:22:45 -0700

    adds validation hooks, fixes net with linear layer output, does 99% accuracy on nodes, and 67% AUC for link prediction, even in this simple model. For Cody/Tanmoy when I am in France

--- graphistry/compute/gnn_utils.py
+++ graphistry/compute/gnn_utils.py
@@ -42,11 +42,16 @@ class LinkPredictionModel(nn.Module):
     def __init__(self, base_model):
         super(LinkPredictionModel, self).__init__()
         self.base_model = base_model
-    
+        self.link_classifier = nn.Linear(256, 1)
+        
     def forward(self, data, edge_index_pos, edge_index_neg):
         x = self.base_model(data)
         x_pos = torch.cat([x[edge_index_pos[0]], x[edge_index_pos[1]]], dim=1)
         x_neg = torch.cat([x[edge_index_neg[0]], x[edge_index_neg[1]]], dim=1)
+        
+        x_pos = self.link_classifier(x_pos).squeeze(-1)
+        x_neg = self.link_classifier(x_neg).squeeze(-1)
+        
         return x_pos, x_neg
 
 # Joint Model for Node and Link Prediction
@@ -75,102 +80,115 @@ def joint_loss(node_pred, node_labels, link_pred_pos, link_pred_neg):
     return node_loss + link_loss
 
 
-def sample_edges(edge_index, num_samples):
+def sample_edges(edge_index, num_samples, balance_degree=False):
     """
-    Sample positive and negative edges considering degree dominance.
-    
+    Sample positive and negative edges from a graph represented by its edge index.
+
     Parameters:
         edge_index (Tensor): The edge index tensor.
-        num_samples (int): The number of positive/negative samples needed.
-        
+        num_samples (int): Number of negative samples to generate.
+        balance_degree (bool): Whether to use degree-based sampling (need to add, as first attempt was sloooow)
+
     Returns:
-        edge_index_pos (Tensor): Tensor for positive samples.
-        edge_index_neg (Tensor): Tensor for negative samples.
+        pos_samples (Tensor): Tensor of positive edge samples.
+        neg_samples (Tensor): Tensor of negative edge samples.
     """
-    # Create an edge list and a degree dictionary
-    edge_list = edge_index.t().tolist()
-    degree_dict = defaultdict(int)
+    print("Step 1: Determine number of nodes in the graph")
+    num_nodes = edge_index.max().item() + 1
 
-    for u, v in edge_list:
-        degree_dict[u] += 1
-        degree_dict[v] += 1
-
-    # Sort the edge list based on node degree (sum of degrees of both nodes)
-    sorted_edge_list = sorted(edge_list, key=lambda x: degree_dict[x[0]] + degree_dict[x[1]])
-
-    # Split into high-degree and low-degree edges
-    mid_point = len(sorted_edge_list) // 2
-    high_degree_edges = sorted_edge_list[mid_point:]
-    low_degree_edges = sorted_edge_list[:mid_point]
-
-    # Sample equally from high-degree and low-degree edges for positive samples
-    positive_samples = random.sample(high_degree_edges, num_samples // 2) + random.sample(low_degree_edges, num_samples // 2)
-    random.shuffle(positive_samples)
-
-    # Generate negative samples ensuring they are not in the graph
-    negative_samples = set()
-    while len(negative_samples) < num_samples:
-        u, v = random.choice(list(degree_dict.keys())), random.choice(list(degree_dict.keys()))
-        if u != v and (u, v) not in edge_list and (v, u) not in edge_list:
-            # Take into account the degree to balance high and low degree nodes
-            if random.random() < (degree_dict[u] + degree_dict[v]) / (2 * sum(degree_dict.values())):
-                negative_samples.add((u, v))
-
-    # Convert to PyTorch tensors
-    edge_index_pos = torch.tensor(positive_samples, dtype=torch.long).t().contiguous()
-    edge_index_neg = torch.tensor(list(negative_samples), dtype=torch.long).t().contiguous()
+    print("Step 2: Select positive samples")
+    # Positive samples
+    pos_samples = edge_index.t()
     
-    return edge_index_pos, edge_index_neg
+    if pos_samples.shape[0] < num_samples:
+        raise ValueError(f"Not enough edges in graph to sample {num_samples} positive samples.")
+
+    pos_samples = pos_samples[:num_samples]
+
+    print(f"Positive samples:\n{len(pos_samples)}")
+
+    print("Step 3: Create an adjacency set for fast lookup")
+    # Create an adjacency set for fast lookup
+    adjacency_set = set([(u.item(), v.item()) for u, v in pos_samples])
+
+    print("Step 4: Perform negative sampling")
+    # Negative sampling
+    neg_samples = set()
+
+    print("Sampling candidate negative samples...")
+    while len(neg_samples) < num_samples:
+        u, v = np.random.randint(0, num_nodes, 2)
+        if u != v and (u, v) not in adjacency_set and (v, u) not in adjacency_set:
+            print(f"Adding negative sample: ({u}, {v})") if u+v %10 ==0 else None
+            neg_samples.add((u, v))
+
+    neg_samples = torch.tensor(list(neg_samples), dtype=torch.long)
+
+    print(f"Negative samples:\n{len(neg_samples)}")
+
+    return pos_samples.t(), neg_samples.t()
 
 if __name__ == '__main__':
     # use Planetoid's CORA dataset as an example.
+
+    # Check if GPU is available
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Load dataset and create train/validation Data objects
     dataset = Planetoid(root='/tmp/Cora', name='Cora')
-    data = dataset[0]
+    data = dataset[0].to(device)
 
-    # Create data loaders (use your own data loaders if you have custom datasets)
-    train_loader = DataLoader([data], batch_size=32, shuffle=True)
-    val_loader = DataLoader([data], batch_size=32, shuffle=False)
+    train_data = data  # For this example, same data for training and validation (usually these should be different)
+    val_data = data
 
+    train_loader = DataLoader([train_data], batch_size=32, shuffle=True)
+    val_loader = DataLoader([val_data], batch_size=32, shuffle=False)
 
-    # Initialize model and optimizer
-    joint_model = JointModel(dataset.num_features)
-    optimizer = optim.Adam(joint_model.parameters(), lr=0.003)
+    joint_model = JointModel(dataset.num_features).to(device)
+    optimizer = optim.Adam(joint_model.parameters(), lr=0.01)
 
-    # Split edges for positive and negative samples using degree dominance
-    edge_index_pos, edge_index_neg = sample_edges(data.edge_index, num_samples=2 * data.num_edges)
-    edge_index_pos_val, edge_index_neg_val = sample_edges(data.edge_index, 100)
+    edge_index_pos_train, edge_index_neg_train = sample_edges(train_data.edge_index, train_data.num_edges)
+    edge_index_pos_val, edge_index_neg_val = sample_edges(val_data.edge_index, 100)
+
+    # Make sure to move sampled edges to the same device as your model
+    edge_index_pos_train, edge_index_neg_train = edge_index_pos_train.to(device), edge_index_neg_train.to(device)
+    edge_index_pos_val, edge_index_neg_val = edge_index_pos_val.to(device), edge_index_neg_val.to(device)
 
     # Training Loop
     joint_model.train()
     for epoch in range(100):
         for batch in train_loader:
             optimizer.zero_grad()
-            
-            node_pred, link_pred_pos, link_pred_neg = joint_model(batch, edge_index_pos, edge_index_neg)
-            
+
+            batch = batch.to(device)
+
+            node_pred, link_pred_pos, link_pred_neg = joint_model(batch, edge_index_pos_train, edge_index_neg_train)
+
             loss = joint_loss(node_pred, batch.y, link_pred_pos, link_pred_neg)
-            
+
             loss.backward()
             optimizer.step()
-            
-            print(f'Epoch {epoch+1}, Loss: {loss.item()}')
 
-    # Evaluation Loop (simplified)
-    joint_model.eval()
-    with torch.no_grad():
-        correct = 0
-        for batch in val_loader:
-            node_pred_val, link_pred_pos_val, link_pred_neg_val = joint_model(batch, edge_index_pos_val, edge_index_neg_val)
-            val_loss = joint_loss(node_pred_val, batch.y, link_pred_pos_val, link_pred_neg_val)
+            print(f'Epoch {epoch+1}, Train Loss: {loss.item()}')
 
-            # Node prediction metrics
-            pred = node_pred_val.argmax(dim=1)
-            node_correct = pred.eq(batch.y).sum().item()
-            node_accuracy = node_correct / len(val_loader.dataset)
-            
+        # Validation Loop
+        joint_model.eval()
+        with torch.no_grad():
+            for batch in val_loader:
+                batch = batch.to(device)
+
+                node_pred_val, link_pred_pos_val, link_pred_neg_val = joint_model(batch, edge_index_pos_val, edge_index_neg_val)
+                val_loss = joint_loss(node_pred_val, batch.y, link_pred_pos_val, link_pred_neg_val)
+
+                # Node prediction metrics
+                pred = node_pred_val.argmax(dim=1)
+                node_correct = pred.eq(batch.y).sum().item()
+                node_accuracy = node_correct / pred.shape[0]
+
             # Link prediction metrics
-            link_labels = torch.cat([torch.ones(link_pred_pos_val.shape[0]), torch.zeros(link_pred_neg_val.shape[0])])
+            link_labels = torch.cat([torch.ones(link_pred_pos_val.shape[0]), torch.zeros(link_pred_neg_val.shape[0])]).to(device)
             link_preds = torch.cat([link_pred_pos_val, link_pred_neg_val])
             roc_score = roc_auc_score(link_labels.detach().cpu(), link_preds.detach().cpu())
 
-            print(f'Epoch {epoch+1}, Validation Loss: {val_loss.item()}, Node Classification Accuracy: {node_accuracy}, Link Prediction ROC: {roc_score}')            
+            print(f'-- Validation Loss: {val_loss.item()}, Node Classification Accuracy: {node_accuracy}, Link Prediction ROC: {roc_score}')
+            print()
