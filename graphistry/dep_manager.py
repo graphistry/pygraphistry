86f51b35f5a5547009b32e881b31361d94db6160
Daniel <dcolinmorgan@gmail.com>
2023-10-12 11:41:09 +0200

    add return types

--- graphistry/dep_manager.py
+++ graphistry/dep_manager.py
@@ -6,8 +6,9 @@ class DepManager:
     def __init__(self):
         self.pkgs = {}
         self.deps()
+        return types
 
-    def __getattr__(self, pkg):
+    def __getattr__(self, pkg:str):
         self._add_deps(pkg)
         try:
             return True, "ok", self.pkgs[pkg], self.pkgs[pkg].__version__
