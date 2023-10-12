8c48dcf339d01175aa2b82c65469a763da16cab3
Daniel <dcolinmorgan@gmail.com>
2023-10-12 11:16:34 +0200

    update umap&feature tests

--- graphistry/feature_utils.py
+++ graphistry/feature_utils.py
@@ -92,7 +92,7 @@ def assert_imported():
         logger.debug(f"SCIPY VERSION: {scipy_version}")
         logger.debug(f"Dirty CAT VERSION: {dirty_cat_version}")
         logger.debug(f"sklearn VERSIOgtN: {sklearn_version}")
-        has_min_dependany_ = True
+        has_min_dependancy_ = True
 
     if not has_min_dependancy_:
         logger.error(  # noqa
