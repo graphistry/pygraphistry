f47b6d7c43f15b9d9666e888c37c7503e5422afd
Daniel <dcolinmorgan@gmail.com>
2023-10-16 18:52:36 +0200

    review leo lint

--- graphistry/dep_manager.py
+++ graphistry/dep_manager.py
@@ -5,13 +5,7 @@ class DepManager:
         self.pkgs = {}
 
     def __getattr__(self, pkg:str):
-        if '_' not in pkg:
-            self._add_deps(pkg)
-            try:
-                return True, "ok", self.pkgs[pkg], self.pkgs[pkg].__version__
-            except KeyError:
-                return False, str(pkg) + " not installed", None, None
-        else:
+        if '_' in pkg:
             module = '.'.join(pkg.split('_')[:-1])
             name = pkg.split('_')[-1]
             self.import_from(module, name)
@@ -19,6 +13,12 @@ class DepManager:
                 return True, "ok", self.pkgs[name], self.pkgs[module].__version
             except KeyError:
                 return False, str([module,name]) + " not installed", None, None
+        else:
+            self._add_deps(pkg)
+            try:
+                return True, "ok", self.pkgs[pkg], self.pkgs[pkg].__version__
+            except KeyError:
+                return False, str(pkg) + " not installed", None, None
 
     def _add_deps(self, pkg:str):
         try:
@@ -26,11 +26,11 @@ class DepManager:
             self.pkgs[pkg] = pkg_val
             # setattr(self, pkg, pkg_val)
         except:
-            setattr(self, pkg, None)
+            pass
 
     def import_from(self,pkg:str, name:str):
         try:
             module = __import__(pkg, fromlist=[name])
             self.pkgs[name] = module
         except:
-            setattr(self, pkg, None)
+            pass
