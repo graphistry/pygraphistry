caecfbaadf2e0acba280677225a7cc4326956112
Daniel <dcolinmorgan@gmail.com>
2023-10-19 15:00:42 +0200

    lint

--- graphistry/embed_utils.py
+++ graphistry/embed_utils.py
@@ -185,9 +185,9 @@ class HeterographEmbedModuleMixin(MIXIN_BASE):
 
     def _train_embedding(self, res, epochs:int, batch_size:int, lr:float, sample_size:int, num_steps:int, device) -> Plottable:
         # _, torch, nn, _, _, _, _, trange = lazy_embed_import_dep()
-        _, _, torch, _ = dep.torch
-        _, _, nn, _ = dep.torch.nn
-        _, _, trange, _ = dep.tqdm.trange
+        _, _, torch, _ = deps.torch
+        _, _, nn, _ = deps.torch.nn
+        _, _, trange, _ = deps.tqdm.trange
         log('Training embedding')
         model, g_dataloader = res._init_model(res, batch_size, sample_size, num_steps, device)
         if hasattr(res, "_embed_model") and not res._build_new_embedding_model:
