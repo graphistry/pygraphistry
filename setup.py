05a1329b24e9a4f303a55a331ca51f8f46fb7b21
Daniel <dcolinmorgan@gmail.com>
2023-12-01 16:11:37 +0800

    better setup install for cucat

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ base_extras_light = {
 }
 
 base_extras_heavy = {
-  'umap-learn': ['umap-learn', 'dirty-cat==0.2.0', 'scikit-learn>=1.0','cu_cat @ git+http://github.com/graphistry/cu-cat.git@v0.06.0'],
+  'umap-learn': ['umap-learn', 'dirty-cat==0.2.0', 'scikit-learn>=1.0','cu_cat @ git+http://github.com/graphistry/cu-cat.git@v0.06.1'],
 }
 # https://github.com/facebookresearch/faiss/issues/1589 for faiss-cpu 1.6.1, #'setuptools==67.4.0' removed
 base_extras_heavy['ai'] = base_extras_heavy['umap-learn'] + ['scipy', 'dgl', 'torch<2', 'sentence-transformers', 'faiss-cpu', 'joblib']
