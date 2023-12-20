32941ddf31e4ac8b73e570047efd8dc93f0fc7d2
Thomas Cook <tcook@graphistry.com>
2023-12-20 14:25:30 -0600

    fix: test change for the register command to use markdown instead of HTML to allow viewing for databricks users

--- graphistry/pygraphistry.py
+++ graphistry/pygraphistry.py
@@ -270,7 +270,8 @@ class PyGraphistry(object):
         if in_ipython() or in_databricks() or sso_opt_into_type == 'display':  # If run in notebook, just display the HTML
             # from IPython.core.display import HTML
             from IPython.display import display, HTML
-            display(HTML(f'<a href="{auth_url}" target="_blank">Login SSO</a>'))
+            display(HTML(f'<a href="{auth_url}" target="_blank">old: Login SSO</a>'))
+            display(Markdown(f'[new: Login SSO]({auth_url})"))
             print("Please click the above URL to open browser to login")
             print(f"If you cannot see the URL, please open browser, browse to this URL: {auth_url}")
             print("Please close browser tab after SSO login to back to notebook")
