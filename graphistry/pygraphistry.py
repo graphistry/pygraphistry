fdd74fcc509b3a77d02777ab1817baecb2eea386
Thomas Cook <tcook@graphistry.com>
2024-03-04 21:53:37 -0600

    added js timer for databricks

--- graphistry/pygraphistry.py
+++ graphistry/pygraphistry.py
@@ -267,7 +267,7 @@ class PyGraphistry(object):
 
         """
 
-        if in_ipython() or in_databricks() or sso_opt_into_type == 'display':  # If run in notebook, just display the HTML
+        if in_databricks():  # If run in databricks notebook, display the HTML and timer using javascript issue #539
             from IPython.display import display, HTML
 
             # this function was required due to bug where databricks notebooks do not display the html link while a timer is running 
@@ -295,7 +295,17 @@ class PyGraphistry(object):
                 display(HTML(html_content))
 
             display_link_and_timer(auth_url, sso_timeout)
+            logger.info("display_link_and_timer() done")
 
+        else: 
+        if in_ipython() or sso_opt_into_type == 'display':  # If run in notebook, just display the HTML
+            from IPython.display import display, HTML
+            display(HTML(f'<a href="{auth_url}" target="_blank">Login SSO</a>'))
+            print("Please click the above URL to open browser to login")
+            print(f"If you cannot see the URL, please open browser, browse to this URL: {auth_url}")
+            print("Please close browser tab after SSO login to back to notebook")
+
+            
         elif sso_opt_into_type == 'browser':
             print("Please minimize browser after your SSO login and go back to pygraphistry")
 
