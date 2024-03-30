b9cfc73cd45b8bb23de0e2b135edc8ba907efe0a
Vaim Dev <dev@v-aim.com>
2024-03-30 16:34:55 +0800

    debug (databricks): revert it back to original code

--- graphistry/pygraphistry.py
+++ graphistry/pygraphistry.py
@@ -321,17 +321,17 @@ class PyGraphistry(object):
                 print("Please run graphistry.sso_get_token() to complete the authentication after you have authenticated via SSO")
                 return None
         else:
-            print("Start getting token ...")
-            token = None
-            for i in range(10):
-                token, org_name = PyGraphistry._sso_get_token()
-                if token:
-                    # set org_name to sso org
-                    PyGraphistry._config['org_name'] = org_name
-                    print("Successfully logged in")
-                    return PyGraphistry.api_token()
-                print("Keep trying to get token ...")
-                time.sleep(5)
+            # print("Start getting token ...")
+            # token = None
+            # for i in range(10):
+            #     token, org_name = PyGraphistry._sso_get_token()
+            #     if token:
+            #         # set org_name to sso org
+            #         PyGraphistry._config['org_name'] = org_name
+            #         print("Successfully logged in")
+            #         return PyGraphistry.api_token()
+            #     print("Keep trying to get token ...")
+            #     time.sleep(5)
 
             print("Please run graphistry.sso_get_token() to complete the authentication")
             return None
