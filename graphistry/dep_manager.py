03093299a3c872947a1f6da4336e16d54acbe442
Daniel <dcolinmorgan@gmail.com>
2024-05-24 19:19:34 +0800

    test gpu-avail

--- graphistry/dep_manager.py
+++ graphistry/dep_manager.py
@@ -1,15 +1,32 @@
 import importlib
+import subprocess
+
+from .constants import GPU_REQ
 
 class DepManager:
     def __init__(self):
         self.pkgs = {}
 
     def __getattr__(self, pkg:str):
-        self._add_deps(pkg)
+        # self._add_deps(pkg)
+        self._proc_import(pkg)
         try:
             return self.pkgs[pkg]
         except KeyError:
             return None
+
+    def _proc_import(self, pkg:str):
+        if pkg in GPU_REQ and self._is_gpu_available():
+            self._add_deps(pkg)
+        elif pkg not in GPU_REQ:
+            self._add_deps(pkg)
+
+    def _is_gpu_available():
+        try:
+            output = subprocess.check_output("nvidia-smi", shell=True)
+            return len(output) > 0
+        except subprocess.CalledProcessError:
+            return False
 
     def _add_deps(self, pkg:str):
         try:
@@ -18,13 +35,5 @@ class DepManager:
             setattr(self, pkg, pkg_val)
         except:
             pass
-
-    def import_from(self,pkg:str, name:str):
-        try:
-            module = __import__(pkg, fromlist=[name])
-            self.pkgs[name] = module
-        except:
-            pass
-
 
 deps = DepManager()
