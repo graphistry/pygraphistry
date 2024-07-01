3a28c8ca06d4345c2e727a5780243415c06bdff8
Daniel <dcolinmorgan@gmail.com>
2024-07-01 11:19:43 +0200

    lint

--- graphistry/tests/test_gfql_utils.py
+++ graphistry/tests/test_gfql_utils.py
@@ -13,7 +13,8 @@ class test_GFQLUtils(unittest.TestCase):
     def grab_fb_dataset_id() -> None:
         ge_df = pd.read_csv('https://raw.githubusercontent.com/graphistry/pygraphistry/master/demos/data/facebook_combined.txt', sep=' ', names=['s', 'd'])
         import graphistry
-        g = graphistry.edges(ge_df, 's', 'd').nodes(gg._nodes, 'id')
+        g = graphistry.edges(ge_df, 's', 'd')
+        g = g.nodes(g._nodes, 'id')
         shareable_and_embeddable_url = g.plot(render=False)
         dataset_id = re.search(r'dataset=([^&]+)&type', shareable_and_embeddable_url)
         return dataset_id.group(1)
