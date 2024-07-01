7d4ca8b438c0b108aa9a6f681c7410762b25d750
Daniel <dcolinmorgan@gmail.com>
2024-07-01 11:22:02 +0200

    lint

--- graphistry/tests/test_gfql_utils.py
+++ graphistry/tests/test_gfql_utils.py
@@ -5,19 +5,18 @@ import unittest
 import pandas as pd
 import pytest
 
-# import graphistry
+import graphistry
 
 class test_GFQLUtils(unittest.TestCase):
 
     @pytest.fixture
     def grab_fb_dataset_id() -> None:
         ge_df = pd.read_csv('https://raw.githubusercontent.com/graphistry/pygraphistry/master/demos/data/facebook_combined.txt', sep=' ', names=['s', 'd'])
-        import graphistry
         g = graphistry.edges(ge_df, 's', 'd')
         g = g.nodes(g._nodes, 'id')
         shareable_and_embeddable_url = g.plot(render=False)
         dataset_id = re.search(r'dataset=([^&]+)&type', shareable_and_embeddable_url)
-        return dataset_id.group(1)
+        return g, dataset_id.group(1)
         
     def null_filter_ex(dataset_id=grab_fb_dataset_id) -> None:
         g.gfql(operations = [{"type": "Edge",
