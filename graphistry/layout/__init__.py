c689b7a7a71bfb9c7594d4197750c0bc04055e44
Leo Meyerovich <leo@graphistry.com>
2024-07-08 16:40:21 -0700

    wip(ring layout): time and continuous

--- graphistry/layout/__init__.py
+++ graphistry/layout/__init__.py
@@ -1,4 +1,5 @@
 #from .utils import *
 #from .graph import *
-from .sugiyama import SugiyamaLayout
 from .gib import group_in_a_box_layout
+from .sugiyama import SugiyamaLayout
+from .ring import time_ring
