6718444b17a29813700efb33ac0ee236671f4fdb
Leo Meyerovich <leo@graphistry.com>
2024-08-03 01:03:57 -0700

    fix(types)

--- graphistry/layout/modularity_weighted/modularity_weighted.py
+++ graphistry/layout/modularity_weighted/modularity_weighted.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Literal, Optional
 import pandas as pd
 
 from graphistry.Engine import Engine, EngineAbstract, resolve_engine
@@ -13,9 +13,9 @@ def modularity_weighted_layout(
   same_community_weight: float = 2.0,
   cross_community_weight: float = 0.3,
   edge_influence: float = 2.0,
-  engine: EngineAbstract = 'auto'
+  engine: EngineAbstract = EngineAbstract.AUTO
 ) -> Plottable:
-  """
+    """
 
     Compute a modularity-weighted layout, where edges are weighted based on whether they connect nodes in the same community or different communities.
 
@@ -66,51 +66,51 @@ def modularity_weighted_layout(
             )
             g.plot()
 
-  """
-  assert g._edges is not None, 'Expected edges to be set'
-  if community_col is None:
-    g = g.materialize_nodes()
-    engine_concrete = resolve_engine(engine, g)
-    if community_alg is None:
-      if engine_concrete == Engine.PANDAS:
-        community_alg = 'community_multilevel'
-      else:
-        community_alg = 'louvain'
-      if community_params is None:
-        community_params = {'directed': False}
-    community_params = community_params or {}
-    if engine_concrete == Engine.PANDAS:
-      g = g.compute_igraph(community_alg, **community_params)
-    elif engine_concrete == Engine.CUDF:
-      import cudf
-      if not isinstance(g._edges, cudf.DataFrame):
-        assert isinstance(g._edges, pd.DataFrame), f'Expected edges to be cudf or pandas, got: {type(g._edges)}'
-        g = g.edges(cudf.DataFrame(g._edges))
-      g = g.compute_cugraph(community_alg, **community_params)
+    """
+    assert g._edges is not None, 'Expected edges to be set'
+    if community_col is None:
+        g = g.materialize_nodes()
+        engine_concrete = resolve_engine(engine, g)
+        if community_alg is None:
+            if engine_concrete == Engine.PANDAS:
+                community_alg = 'community_multilevel'
+            else:
+                community_alg = 'louvain'
+            if community_params is None:
+                community_params = {'directed': False}
+        community_params = community_params or {}
+        if engine_concrete == Engine.PANDAS:
+            g = g.compute_igraph(community_alg, **community_params)  # type: ignore
+        elif engine_concrete == Engine.CUDF:
+            import cudf
+            if not isinstance(g._edges, cudf.DataFrame):
+                assert isinstance(g._edges, pd.DataFrame), f'Expected edges to be cudf or pandas, got: {type(g._edges)}'
+                g = g.edges(cudf.DataFrame(g._edges))
+            g = g.compute_cugraph(community_alg, **community_params)  # type: ignore
+        else:
+            raise ValueError(f'Unsupported engine: {engine}')
+        community_col = community_alg
     else:
-      raise ValueError(f'Unsupported engine: {engine}')
-    community_col = community_alg
-  else:
-    assert community_col in g._nodes, f'Expected community column {community_col} in nodes, only available are {g._nodes.columns}'
+        assert community_col in g._nodes, f'Expected community column {community_col} in nodes, only available are {g._nodes.columns}'
 
-  g = g.layout_settings(edge_influence=edge_influence)
+    g = g.layout_settings(edge_influence=edge_influence)
 
-  assert 'source_community' not in g._edges, 'Expected no source_community column in edges'
-  assert 'destination_community' not in g._edges, 'Expected no destination_community column in edges'
+    assert 'source_community' not in g._edges, 'Expected no source_community column in edges'
+    assert 'destination_community' not in g._edges, 'Expected no destination_community column in edges'
 
-  edges = (g._edges
-    .merge(g._nodes[[community_col, g._node]], left_on=g._source, right_on=g._node).rename(columns={community_col: 'source_community'}).drop(columns=[g._node])
-    .merge(g._nodes[[community_col, g._node]], left_on=g._destination, right_on=g._node).rename(columns={community_col: 'destination_community'}).drop(columns=[g._node])
-  )
+    edges = (g._edges
+        .merge(g._nodes[[community_col, g._node]], left_on=g._source, right_on=g._node).rename(columns={community_col: 'source_community'}).drop(columns=[g._node])
+        .merge(g._nodes[[community_col, g._node]], left_on=g._destination, right_on=g._node).rename(columns={community_col: 'destination_community'}).drop(columns=[g._node])
+    )
 
-  same_community=(edges['source_community'] == edges['destination_community'])
-  edges = edges.assign(
-      weight=same_community.map({
-          True: same_community_weight,
-          False: cross_community_weight
-      }),
-      same_community=same_community
-  )
-  edges = edges.drop(columns=['source_community', 'destination_community'])
+    same_community = (edges['source_community'] == edges['destination_community'])
+    edges = edges.assign(
+        weight=same_community.map({
+            True: same_community_weight,
+            False: cross_community_weight
+        }),
+        same_community=same_community
+    )
+    edges = edges.drop(columns=['source_community', 'destination_community'])
 
-  return g.edges(edges).bind(edge_weight='weight')
+    return g.edges(edges).bind(edge_weight='weight')
