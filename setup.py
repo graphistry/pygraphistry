4398b61403f2c0f0aefbc74b2bac0268522125bd
Leo Meyerovich <leo@graphistry.com>
2024-10-17 01:29:42 -0700

    infra(setup.py): move graphviz to heavy bc apt-get

--- setup.py
+++ setup.py
@@ -50,12 +50,12 @@ base_extras_light = {
     'gremlin': ['gremlinpython'],
     'bolt': ['neo4j', 'neotime'],
     'nodexl': ['openpyxl==3.1.0', 'xlrd'],
-    'pygraphviz': ['pygraphviz'],
     'jupyter': ['ipython'],
 }
 
 base_extras_heavy = {
   'umap-learn': ['umap-learn', 'dirty-cat==0.2.0', 'scikit-learn>=1.0'],
+  'pygraphviz': ['pygraphviz'],  # + apt-get graphviz, graphviz-dev
 }
 # https://github.com/facebookresearch/faiss/issues/1589 for faiss-cpu 1.6.1, #'setuptools==67.4.0' removed
 base_extras_heavy['ai'] = base_extras_heavy['umap-learn'] + ['scipy', 'dgl', 'torch<2', 'sentence-transformers', 'faiss-cpu', 'joblib']
