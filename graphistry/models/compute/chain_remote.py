aa9e94776276347e91e670f849e41fb6f76fbefd
Leo Meyerovich <leo@graphistry.com>
2024-12-08 19:49:00 -0800

    fix(gfql remote): missing file

--- graphistry/models/compute/chain_remote.py
+++ graphistry/models/compute/chain_remote.py
@@ -2,4 +2,7 @@ from typing_extensions import Literal
 
 
 OutputType = Literal["all", "nodes", "edges", "shape"]
+output_type_values = {"all", "nodes", "edges", "shape"}
+
 FormatType = Literal["json", "csv", "parquet"]
+from_type_values = {"json", "csv", "parquet"}
