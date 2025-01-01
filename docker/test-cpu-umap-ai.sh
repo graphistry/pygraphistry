5f66b35e525b60dce4013a7a4ef1fdd6490e31e7
Leo Meyerovich <leo@graphistry.com>
2024-12-31 20:17:14 -0800

    infra(umap testers): full pytest overriding

--- docker/test-cpu-umap-ai.sh
+++ docker/test-cpu-umap-ai.sh
@@ -2,6 +2,8 @@
 set -ex
 
 
+TEST_FILES=${@:-"graphistry/tests/test_feature_utils.py graphistry/tests/test_umap_utils.py"}
+
 PYTHON_VERSION=${PYTHON_VERSION:-3.9} \
 PIP_DEPS=${PIP_DEPS:--e .[ai,test,testai]} \
 WITH_LINT=${WITH_LINT:-1} \
@@ -11,6 +13,4 @@ WITH_TEST=${WITH_TEST:-1} \
 SENTENCE_TRANSFORMER=${SENTENCE_TRANSFORMER-average_word_embeddings_komninos} \
 SENTENCE_TRANSFORMER=${SENTENCE_TRANSFORMER} \
     ./test-cpu-local.sh \
-        graphistry/tests/test_feature_utils.py \
-        graphistry/tests/test_umap_utils.py \
-        $@
+        $TEST_FILES
