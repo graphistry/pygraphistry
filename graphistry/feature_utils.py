95c6a9b124dac341848992a60cd93cca7a0ac7ab
Leo Meyerovich <leo@graphistry.com>
2025-01-04 20:11:24 -0800

    fix(feat utils): python 3.9 typing

--- graphistry/feature_utils.py
+++ graphistry/feature_utils.py
@@ -321,6 +321,7 @@ def drop_duplicates_with_warning(df: pd.DataFrame) -> pd.DataFrame:
             f"Duplicate columns found: {duplicates}, using first of each."
         )
         df = df.loc[:, ~duplicate_cols]
+        df = df[ [c for c, dupe in zip(df.columns, duplicate_cols) if not dupe] ]
     return df
 
 
