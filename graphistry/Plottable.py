032dc0a56366d595e552d032305ae540d7b089b3
Thomas Cook <tcook@graphistry.com>
2025-01-22 16:38:03 -0600

    add support for google spanner graph (#622)
    * (feature): new support for Google Spanner Graph
    
    * renamed spannergraph file
    
    * changed dir for spannergraph.py, added module in setup.py
    
    * added lazy imports
    
    * removed import from Plotterbase.py
    
    * fix pydocs
    
    * remove database_clients dir, moved to plugins/
    
    * various changes to pass in spanner config to register()
    
    * added debugging
    
    * added demo notebook, debug output
    
    * minor changes
    
    * various changes for error handling, imports and pydocs
    
    * fixed register and uncomment gcloud
    
    * fixed typo in register
    
    * added spanner_query_to_df and other fixes from PR comments
    
    * updated notebook with more examples
    
    * fix lint issues
    
    * fix linting errors
    
    * fix linting errors
    
    * fix more lint issue
    
    * fix more lint issue
    
    * fix more lint issue
    
    * fix more lint issue
    
    * updated notebook with CTA and other docs
    
    * fix for readthedocs markdown
    
    * updates from PR comments
    
    * removed None assignment for _spannergraph - per PR comments
    
    * changes to pass Plottable dynamically to SpannerGraph.gql_to_g
    
    * various PR review changes
    
    * fix lint error and add plot output back to notebook
    
    * fix lint error and add plot output back to notebook
    
    * fix lint error about blank line at end of file

--- graphistry/Plottable.py
+++ graphistry/Plottable.py
@@ -62,7 +62,8 @@ class Plottable(object):
     _complex_encodings : dict
     _bolt_driver : Any
     _tigergraph : Any
-
+    _spannergraph: Any
+    
     _dataset_id: Optional[str]
     _url: Optional[str]
     _nodes_file_id: Optional[str]
