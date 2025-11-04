572daf42927ce103ee1e137c36bd946049c20a0d
Andrew Horwitz <ahwitz@gmail.com>
2025-11-04 12:56:27 +0800

    dev(compute-docs): updated top-level __init__.py imports

--- graphistry/__init__.py
+++ graphistry/__init__.py
@@ -63,6 +63,7 @@ from graphistry.pygraphistry import (  # noqa: E402, F401
 
 from graphistry.compute import (
     n, e, e_forward, e_reverse, e_undirected,
+    let, ref,
     Chain,
     call, CallMethodName,
 
