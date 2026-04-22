ade5d802e0303c3bcb3fe7717a7d2455356b47ec
Leo Meyerovich <leo@graphistry.com>
2026-04-21 22:26:14 -0700

    Add GFQL predicate pushdown pass and route hook (#1187)

--- graphistry/compute/gfql/passes/__init__.py
+++ graphistry/compute/gfql/passes/__init__.py
@@ -1,10 +1,12 @@
 """Logical plan pass framework."""
 
 from .manager import DEFAULT_LOGICAL_PASSES, LogicalPass, PassManager, PassResult
+from .predicate_pushdown import PredicatePushdownPass
 
 __all__ = [
     "DEFAULT_LOGICAL_PASSES",
     "LogicalPass",
     "PassManager",
     "PassResult",
+    "PredicatePushdownPass",
 ]
